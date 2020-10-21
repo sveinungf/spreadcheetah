a15e9698914c6a1f74d8bd8aab4db026bea885da
ikonage <ikonage@gmail.com>
2020-10-21 18:37:50 +0200

    StyledCell as struct

--- SpreadCheetah/StyledCell.cs
+++ SpreadCheetah/StyledCell.cs
@@ -1,8 +1,10 @@
 using SpreadCheetah.Styling;
+using System;
+using System.Collections.Generic;
 
 namespace SpreadCheetah
 {
-    public class StyledCell
+    public readonly struct StyledCell : IEquatable<StyledCell>
     {
         public Cell Cell { get; }
         public StyleId? StyleId { get; }
@@ -12,5 +14,26 @@ namespace SpreadCheetah
             Cell = cell;
             StyleId = styleId;
         }
+
+        public bool Equals(StyledCell other)
+        {
+            return Cell.Equals(other.Cell) && EqualityComparer<StyleId?>.Default.Equals(StyleId, other.StyleId);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is StyledCell other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1472682488;
+            hashCode = hashCode * -1521134295 + Cell.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<StyleId?>.Default.GetHashCode(StyleId);
+            return hashCode;
+        }
+
+        public static bool operator ==(StyledCell left, StyledCell right) => left.Equals(right);
+        public static bool operator !=(StyledCell left, StyledCell right) => !left.Equals(right);
     }
 }
