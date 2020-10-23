c459f4db6ba52c970d97f598d89af603c6401af9
ikonage <ikonage@gmail.com>
2020-10-23 23:07:57 +0200

    Write column widths

--- SpreadCheetah/Worksheet.cs
+++ SpreadCheetah/Worksheet.cs
@@ -4,6 +4,7 @@ using SpreadCheetah.Worksheets;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,9 +14,12 @@ namespace SpreadCheetah
     {
         private const string SheetHeader =
             "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
-            "<worksheet xmlns=\"http://schemas.openxmlformats.org/spreadsheetml/2006/main\">" +
-            "<sheetData>";
+            "<worksheet xmlns=\"http://schemas.openxmlformats.org/spreadsheetml/2006/main\">";
 
+        private const string ColsBegin = "<cols>";
+        private const string ColsEnd = "</cols>";
+        private const string SheetDataBegin = "<sheetData>";
+        private const string ColsEndSheetDataBegin = ColsEnd + SheetDataBegin;
         private const string SheetFooter = "</sheetData></worksheet>";
 
         private readonly Stream _stream;
@@ -35,8 +39,40 @@ namespace SpreadCheetah
 
         public async ValueTask WriteHeadAsync(WorksheetOptions? options, CancellationToken token)
         {
-            // TODO: Write column options
             _buffer.Index += Utf8Helper.GetBytes(SheetHeader, _buffer.GetNextSpan());
+            if (options == null)
+            {
+                _buffer.Index += Utf8Helper.GetBytes(SheetDataBegin, _buffer.GetNextSpan());
+                return;
+            }
+
+            _buffer.Index += Utf8Helper.GetBytes(ColsBegin, _buffer.GetNextSpan());
+
+            var sb = new StringBuilder();
+            foreach (var keyValuePair in options.ColumnOptions)
+            {
+                var column = keyValuePair.Value;
+                if (column.Width == null) continue;
+
+                sb.Clear();
+                sb.Append("<col min=\"");
+                sb.Append(keyValuePair.Key);
+                sb.Append("\" max=\"");
+                sb.Append(keyValuePair.Key);
+                sb.Append("\" width=\"");
+                sb.Append(column.Width.Value);
+                sb.Append("\" customWidth=\"1\" />");
+
+                if (sb.Length > _buffer.GetRemainingBuffer())
+                    await _buffer.FlushToStreamAsync(_stream, token).ConfigureAwait(false);
+
+                _buffer.Index += Utf8Helper.GetBytes(sb.ToString(), _buffer.GetNextSpan());
+            }
+
+            if (ColsEndSheetDataBegin.Length > _buffer.GetRemainingBuffer())
+                await _buffer.FlushToStreamAsync(_stream, token).ConfigureAwait(false);
+
+            _buffer.Index += Utf8Helper.GetBytes(ColsEndSheetDataBegin, _buffer.GetNextSpan());
         }
 
         public bool TryAddRow(IList<Cell> cells, out int currentIndex) => _dataCellWriter.TryAddRow(cells, _nextRowIndex++, out currentIndex);
