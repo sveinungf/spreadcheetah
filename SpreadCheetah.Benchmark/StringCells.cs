798becc00605ad364bfbb5be47c3f503601d8b60
ikonage <ikonage@gmail.com>
2020-11-15 13:23:57 +0100

    Rewrite benchmarks to include creating cells in measurements

--- SpreadCheetah.Benchmark/StringCells.cs
+++ SpreadCheetah.Benchmark/StringCells.cs
@@ -5,7 +5,7 @@ using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using OfficeOpenXml;
-using SpreadCheetah.Benchmark.Helpers;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Packaging;
@@ -13,72 +13,74 @@ using System.Linq;
 using System.Threading.Tasks;
 using OpenXmlCell = DocumentFormat.OpenXml.Spreadsheet.Cell;
 
-#pragma warning disable CA1001 // Using Cleanup instead of Dispose
-
 namespace SpreadCheetah.Benchmark
 {
     [SimpleJob(RuntimeMoniker.Net48)]
     [SimpleJob(RuntimeMoniker.NetCoreApp31)]
     [SimpleJob(RuntimeMoniker.NetCoreApp50)]
     [MemoryDiagnoser]
-    public class StringCells
+    public class StringCells : IDisposable
     {
-        private const int NumberOfColumns = 10;
         private const string SheetName = "Book1";
 
-        private List<List<DataCell>> _cells = null!;
-        private List<string> _epplusRows = null!;
-        private List<List<OpenXmlCell>> _openXmlDomCells = null!;
-        private List<List<Text>> _openXmlSaxCells = null!;
-        private List<List<string>> _stringValues = null!;
-        private Stream _stream = null!;
+        public Stream Stream { get; set; } = null!;
+        public IList<IList<string>> Values { get; private set; } = null!;
 
         [Params(20000)]
         public int NumberOfRows { get; set; }
 
+        public int NumberOfColumns { get; set; } = 10;
+
         [GlobalSetup]
         public void GlobalSetup()
         {
-            _cells = new List<List<DataCell>>(NumberOfRows);
-            _epplusRows = new List<string>(NumberOfRows);
-            _stringValues = new List<List<string>>(NumberOfRows);
-            _openXmlSaxCells = new List<List<Text>>(NumberOfRows);
-
+            Values = new List<IList<string>>(NumberOfRows);
             for (var row = 1; row <= NumberOfRows; ++row)
             {
-                var columns = Enumerable.Range(1, NumberOfColumns).ToList();
-                _cells.Add(columns.Select(x => new DataCell($"{x}-{row}")).ToList());
-                _epplusRows.Add(string.Join(",", columns.Select(x => $"{x}-{row}")));
-                _openXmlSaxCells.Add(columns.Select(x => new Text($"{x}-{row}")).ToList());
-                _stringValues.Add(columns.Select(x => $"{x}-{row}").ToList());
+                var cells = new List<string>(NumberOfColumns);
+                Values.Add(cells);
+                for (var col = 1; col <= NumberOfColumns; ++col)
+                {
+                    cells.Add($"{col}-{row}");
+                }
             }
         }
 
         [IterationSetup]
         public void IterationSetup()
         {
-            _openXmlDomCells = new List<List<OpenXmlCell>>(NumberOfRows);
-
-            for (var row = 1; row <= NumberOfRows; ++row)
-            {
-                var openXmlCells = Enumerable.Range(1, NumberOfColumns).Select(x => OpenXmlHelper.CreateCell($"{x}-{row}")).ToList();
-                _openXmlDomCells.Add(openXmlCells);
-            }
-
-            _stream = new MemoryStream(2_000_000);
+            Stream = new MemoryStream(2_000_000);
         }
 
         [IterationCleanup]
-        public void IterationCleanup() => _stream.Dispose();
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing) return;
+            Stream?.Dispose();
+            Stream = null!;
+        }
 
         [Benchmark]
         public async Task SpreadCheetah()
         {
-            using var spreadsheet = await Spreadsheet.CreateNewAsync(_stream);
+            using var spreadsheet = await Spreadsheet.CreateNewAsync(Stream);
             await spreadsheet.StartWorksheetAsync(SheetName);
-            for (var row = 1; row <= NumberOfRows; ++row)
+            var cells = new DataCell[NumberOfColumns];
+
+            for (var row = 0; row < Values.Count; ++row)
             {
-                var cells = _cells[row - 1];
+                var rowValues = Values[row];
+                for (var col = 0; col < rowValues.Count; ++col)
+                {
+                    cells[col] = new DataCell(rowValues[col]);
+                }
+
                 await spreadsheet.AddRowAsync(cells);
             }
 
@@ -88,15 +90,17 @@ namespace SpreadCheetah.Benchmark
         [Benchmark]
         public void EpPlus4()
         {
-            using var package = new ExcelPackage(_stream) { Compression = CompressionLevel.BestSpeed };
+            using var package = new ExcelPackage(Stream) { Compression = CompressionLevel.BestSpeed };
             var worksheet = package.Workbook.Worksheets.Add(SheetName);
             var dataTypes = Enumerable.Repeat(eDataTypes.String, NumberOfColumns).ToArray();
             var excelTextFormat = new ExcelTextFormat { DataTypes = dataTypes };
 
-            for (var row = 1; row <= NumberOfRows; ++row)
+            // TODO: Try other approaches in EPPlus
+            for (var row = 0; row < Values.Count; ++row)
             {
-                var cells = worksheet.Cells[row, 1];
-                var rowText = _epplusRows[row - 1];
+                var rowValues = Values[row];
+                var rowText = string.Join(",", rowValues);
+                var cells = worksheet.Cells[row + 1, 1];
                 cells.LoadFromText(rowText, excelTextFormat);
             }
 
@@ -106,7 +110,7 @@ namespace SpreadCheetah.Benchmark
         [Benchmark]
         public void OpenXmlSax()
         {
-            using var xl = SpreadsheetDocument.Create(_stream, SpreadsheetDocumentType.Workbook);
+            using var xl = SpreadsheetDocument.Create(Stream, SpreadsheetDocumentType.Workbook);
             xl.CompressionOption = CompressionOption.SuperFast;
             xl.AddWorkbookPart();
             var wsp = xl.WorkbookPart.AddNewPart<WorksheetPart>();
@@ -120,17 +124,17 @@ namespace SpreadCheetah.Benchmark
             var cell = new OpenXmlCell();
             var inlineString = new InlineString();
 
-            for (var row = 1; row <= NumberOfRows; ++row)
+            for (var row = 0; row < NumberOfRows; ++row)
             {
-                var rowAttributes = new[] { new OpenXmlAttribute("r", null, row.ToString()) };
+                var rowAttributes = new[] { new OpenXmlAttribute("r", null, (row + 1).ToString()) };
                 oxw.WriteStartElement(rowObject, rowAttributes);
-                var cells = _openXmlSaxCells[row - 1];
+                var rowValues = Values[row];
 
-                for (var col = 1; col <= NumberOfColumns; ++col)
+                for (var col = 0; col < rowValues.Count; ++col)
                 {
                     oxw.WriteStartElement(cell, cellAttributes);
                     oxw.WriteStartElement(inlineString);
-                    oxw.WriteElement(cells[col - 1]);
+                    oxw.WriteElement(new Text(rowValues[col]));
                     oxw.WriteEndElement();
                     oxw.WriteEndElement();
                 }
@@ -162,7 +166,7 @@ namespace SpreadCheetah.Benchmark
         [Benchmark]
         public void OpenXmlDom()
         {
-            using var xl = SpreadsheetDocument.Create(_stream, SpreadsheetDocumentType.Workbook);
+            using var xl = SpreadsheetDocument.Create(Stream, SpreadsheetDocumentType.Workbook);
             xl.CompressionOption = CompressionOption.SuperFast;
             var workbookpart = xl.AddWorkbookPart();
             workbookpart.Workbook = new Workbook();
@@ -179,10 +183,21 @@ namespace SpreadCheetah.Benchmark
                 Name = SheetName
             };
 
-            for (var row = 1; row <= NumberOfRows; ++row)
+            var cells = new OpenXmlCell[NumberOfColumns];
+
+            for (var row = 0; row < Values.Count; ++row)
             {
-                var cells = _openXmlDomCells[row - 1];
-                var rowObject = new Row(cells) { RowIndex = (uint)row };
+                var rowValues = Values[row];
+                for (var col = 0; col < rowValues.Count; ++col)
+                {
+                    var inlineString = new InlineString();
+                    inlineString.AppendChild(new Text(rowValues[col]));
+                    var cell = new OpenXmlCell { DataType = CellValues.InlineString };
+                    cell.AppendChild(inlineString);
+                    cells[col] = cell;
+                }
+
+                var rowObject = new Row(cells) { RowIndex = (uint)row + 1 };
                 sheetData.AppendChild(rowObject);
             }
 
@@ -197,16 +212,16 @@ namespace SpreadCheetah.Benchmark
             using var workbook = new XLWorkbook(XLEventTracking.Disabled);
             var worksheet = workbook.Worksheets.Add(SheetName);
 
-            for (var row = 1; row <= NumberOfRows; ++row)
+            for (var row = 0; row < Values.Count; ++row)
             {
-                var columns = _stringValues[row - 1];
-                for (var col = 1; col <= NumberOfColumns; ++col)
+                var rowValues = Values[row];
+                for (var col = 0; col < rowValues.Count; ++col)
                 {
-                    worksheet.Cell(row, col).SetValue(columns[col - 1]);
+                    worksheet.Cell(row + 1, col + 1).SetValue(rowValues[col]);
                 }
             }
 
-            workbook.SaveAs(_stream);
+            workbook.SaveAs(Stream);
         }
     }
 }
