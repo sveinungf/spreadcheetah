7a343e943c2e95e22c84e16239444e83bd8401bf
ikonage <ikonage@gmail.com>
2020-12-28 15:39:25 +0100

    Improve readability of source generator

---
+++ SpreadCheetah.SourceGenerator/Helpers/StringBuilderExtensions.cs
@@ -0,0 +1,12 @@
+using System.Text;
+
+namespace SpreadCheetah.SourceGenerator.Helpers
+{
+    internal static class StringBuilderExtensions
+    {
+        public static StringBuilder AppendLine(this StringBuilder sb, int indentationLevel, string value)
+        {
+            return sb.Append(new string(' ', 4 * indentationLevel)).AppendLine(value);
+        }
+    }
+}
