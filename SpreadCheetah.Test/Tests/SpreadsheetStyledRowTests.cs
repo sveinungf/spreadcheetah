dfaebb4313bf20cd367628e022f85761f5618a17
ikonage <ikonage@gmail.com>
2021-04-04 11:02:36 +0200

    Remove redundant test

--- SpreadCheetah.Test/Tests/SpreadsheetStyledRowTests.cs
+++ SpreadCheetah.Test/Tests/SpreadsheetStyledRowTests.cs
@@ -12,39 +12,6 @@ namespace SpreadCheetah.Test.Tests
     public class SpreadsheetStyledRowTests
     {
         [Theory]
-        [InlineData("OneWord")]
-        [InlineData("With whitespace")]
-        [InlineData("With trailing whitespace ")]
-        [InlineData(" With leading whitespace")]
-        [InlineData("With-Special-Characters!#¤%&")]
-        [InlineData("With\"Quotation\"Marks")]
-        [InlineData("WithNorwegianCharactersÆØÅ")]
-        [InlineData("WithEmoji\ud83d\udc4d")]
-        [InlineData("")]
-        [InlineData(null)]
-        public async Task Spreadsheet_AddRow_NullStyleIdCellWithStringValue(string? value)
-        {
-            // Arrange
-            using var stream = new MemoryStream();
-            await using (var spreadsheet = await Spreadsheet.CreateNewAsync(stream))
-            {
-                await spreadsheet.StartWorksheetAsync("Sheet");
-                var styledCell = new StyledCell(value, null);
-
-                // Act
-                await spreadsheet.AddRowAsync(styledCell);
-                await spreadsheet.FinishAsync();
-            }
-
-            // Assert
-            stream.Position = 0;
-            using var workbook = new XLWorkbook(stream);
-            var worksheet = workbook.Worksheets.Single();
-            var actualCell = worksheet.Cell(1, 1);
-            Assert.Equal(value ?? string.Empty, actualCell.Value);
-        }
-
-        [Theory]
         [InlineData(true)]
         [InlineData(false)]
         public async Task Spreadsheet_AddRow_BoldCellWithStringValue(bool bold)
