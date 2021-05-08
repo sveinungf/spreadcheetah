ff8c2bd4d3c7b248d874686c31cab9f00482a8fd
ikonage <ikonage@gmail.com>
2021-05-08 18:21:32 +0200

    Implement Equals/HashCode for Formula

--- SpreadCheetah/Formula.cs
+++ SpreadCheetah/Formula.cs
@@ -1,15 +1,40 @@
+using System;
 using System.Net;
 
 namespace SpreadCheetah
 {
-    public readonly struct Formula
+    /// <summary>
+    /// Represents a formula for a worksheet cell.
+    /// </summary>
+    public readonly struct Formula : IEquatable<Formula>
     {
-        // TODO: Without '='
-        public string FormulaText { get; }
+        internal string FormulaText { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Formula"/> struct with a formula text.
+        /// The formula text should be without a starting equal sign (=).
+        /// </summary>
         public Formula(string? formulaText)
         {
             FormulaText = formulaText != null ? WebUtility.HtmlEncode(formulaText) : string.Empty;
         }
+
+        /// <inheritdoc/>
+        public bool Equals(Formula other)
+        {
+            return string.Equals(FormulaText, other.FormulaText, StringComparison.Ordinal);
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object? obj) => obj is Formula formula && Equals(formula);
+
+        /// <inheritdoc/>
+        public override int GetHashCode() => HashCode.Combine(FormulaText);
+
+        /// <summary>Determines whether two instances have the same value.</summary>
+        public static bool operator ==(Formula left, Formula right) => left.Equals(right);
+
+        /// <summary>Determines whether two instances have different values.</summary>
+        public static bool operator !=(Formula left, Formula right) => !left.Equals(right);
     }
 }
