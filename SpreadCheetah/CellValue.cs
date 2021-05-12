c975c4f8b57a060a80f670a62a8145568472517c
ikonage <ikonage@gmail.com>
2021-05-13 00:22:30 +0200

    SkipInit in CellValue

--- SpreadCheetah/CellValue.cs
+++ SpreadCheetah/CellValue.cs
@@ -9,8 +9,28 @@ namespace SpreadCheetah
         [FieldOffset(0)] public readonly float FloatValue;
         [FieldOffset(0)] public readonly double DoubleValue;
 
+#if NET5_0_OR_GREATER
+        public CellValue(int value)
+        {
+            System.Runtime.CompilerServices.Unsafe.SkipInit(out this);
+            IntValue = value;
+        }
+
+        public CellValue(float value)
+        {
+            System.Runtime.CompilerServices.Unsafe.SkipInit(out this);
+            FloatValue = value;
+        }
+
+        public CellValue(double value)
+        {
+            System.Runtime.CompilerServices.Unsafe.SkipInit(out this);
+            DoubleValue = value;
+        }
+#else
         public CellValue(int value) : this() => IntValue = value;
         public CellValue(float value) : this() => FloatValue = value;
         public CellValue(double value) : this() => DoubleValue = value;
+#endif
     }
 }
