f1d9f0bc37026e7bddd03a1bafdb3a5698019e71
sveinungf <ikonage@gmail.com>
2022-02-05 19:41:56 +0100

    Integer data validation with all operators

--- SpreadCheetah/Validations/DataValidation.cs
+++ SpreadCheetah/Validations/DataValidation.cs
@@ -5,6 +5,8 @@ namespace SpreadCheetah.Validations;
 
 public sealed class DataValidation
 {
+    private const string MinGreaterThanMaxMessage = "The min value must be less than or equal to the max value.";
+
     internal ValidationType Type { get; }
     internal ValidationOperator Operator { get; }
     internal string? Value1 { get; }
@@ -14,7 +16,7 @@ public sealed class DataValidation
         ValidationType type,
         ValidationOperator op,
         string? value1,
-        string? value2)
+        string? value2 = null)
     {
         Type = type;
         Operator = op;
@@ -40,23 +42,25 @@ public sealed class DataValidation
 
     private ValidationErrorType _errorType;
 
-    public static DataValidation IntegerBetween(int minimum, int maximum)
+    private static DataValidation Integer(ValidationOperator op, int value1, int? value2 = null)
     {
-        // TODO: Validate arguments
-        return new DataValidation(
-            ValidationType.Integer,
-            ValidationOperator.Between,
-            minimum.ToString(CultureInfo.InvariantCulture),
-            maximum.ToString(CultureInfo.InvariantCulture));
+        return new DataValidation(ValidationType.Integer, op,
+            value1.ToString(CultureInfo.InvariantCulture),
+            value2?.ToString(CultureInfo.InvariantCulture));
     }
 
-    public static DataValidation IntegerNotBetween(int minimum, int maximum)
-    {
-        // TODO: Validate arguments
-        return new DataValidation(
-            ValidationType.Integer,
-            ValidationOperator.NotBetween,
-            minimum.ToString(CultureInfo.InvariantCulture),
-            maximum.ToString(CultureInfo.InvariantCulture));
-    }
+    public static DataValidation IntegerBetween(int min, int max) => max < min
+        ? throw new ArgumentException(MinGreaterThanMaxMessage, nameof(min))
+        : Integer(ValidationOperator.Between, min, max);
+
+    public static DataValidation IntegerNotBetween(int min, int max) => max < min
+        ? throw new ArgumentException(MinGreaterThanMaxMessage, nameof(min))
+        : Integer(ValidationOperator.NotBetween, min, max);
+
+    public static DataValidation IntegerEqualTo(int value) => Integer(ValidationOperator.EqualTo, value);
+    public static DataValidation IntegerNotEqualTo(int value) => Integer(ValidationOperator.NotEqualTo, value);
+    public static DataValidation IntegerGreaterThan(int value) => Integer(ValidationOperator.GreaterThan, value);
+    public static DataValidation IntegerGreaterThanOrEqualTo(int value) => Integer(ValidationOperator.GreaterThanOrEqualTo, value);
+    public static DataValidation IntegerLessThan(int value) => Integer(ValidationOperator.LessThan, value);
+    public static DataValidation IntegerLessThanOrEqualTo(int value) => Integer(ValidationOperator.LessThanOrEqualTo, value);
 }
