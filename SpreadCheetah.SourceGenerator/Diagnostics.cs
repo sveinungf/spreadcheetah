4d67af79b5e35de218fa042fe9a471afec924f3b
sveinungf <ikonage@gmail.com>
2022-02-26 13:28:19 +0100

    Avoid using target-typed new expressions for diagnostics due to https://github.com/dotnet/roslyn-analyzers/issues/5828

--- SpreadCheetah.SourceGenerator/Diagnostics.cs
+++ SpreadCheetah.SourceGenerator/Diagnostics.cs
@@ -6,7 +6,7 @@ internal static class Diagnostics
 {
     private const string Category = "SpreadCheetah.SourceGenerator";
 
-    public static readonly DiagnosticDescriptor NoPropertiesFound = new(
+    public static readonly DiagnosticDescriptor NoPropertiesFound = new DiagnosticDescriptor(
         id: "SPCH1001",
         title: "Missing properties with public getters",
         messageFormat: "The type '{0}' has no properties with public getters. This will cause an empty row to be added.",
@@ -14,7 +14,7 @@ internal static class Diagnostics
         DiagnosticSeverity.Warning,
         isEnabledByDefault: true);
 
-    public static readonly DiagnosticDescriptor UnsupportedTypeForCellValue = new(
+    public static readonly DiagnosticDescriptor UnsupportedTypeForCellValue = new DiagnosticDescriptor(
         id: "SPCH1002",
         title: "Unsupported type for cell value",
         messageFormat: "The type '{0}' has a property of type '{1}' which is not supported as a cell value. The property will be ignored when creating the row.",
