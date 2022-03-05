e53c6c7839ffd2b6c7f292cbaee6ec4bf965aff7
sveinungf <ikonage@gmail.com>
2022-03-05 17:07:37 +0100

    XML comments for public data validation code

--- SpreadCheetah/Validations/DataValidation.cs
+++ SpreadCheetah/Validations/DataValidation.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace SpreadCheetah.Validations;
 
+/// <summary>
+/// Represents data validation for one or more worksheet cells.
+/// </summary>
 public sealed class DataValidation
 {
     private const string MinGreaterThanMaxMessage = "The min value must be less than or equal to the max value.";
@@ -28,19 +31,26 @@ public sealed class DataValidation
         Value2 = value2;
     }
 
+    /// <summary>Ignore cells without values. Defaults to <c>true</c>.</summary>
     public bool IgnoreBlank { get; set; } = true;
+    /// <summary>Show an error alert for invalid values. Defaults to <c>true</c>.</summary>
     public bool ShowErrorAlert { get; set; } = true;
+    /// <summary>Show the input message box. Defaults to <c>true</c>.</summary>
     public bool ShowInputMessage { get; set; } = true;
 
+    /// <summary>Title for error alerts. Maximum 32 characters.</summary>
     public string? ErrorTitle { get => _errorTitle; set => _errorTitle = VerifyLength(value, 32); }
     private string? _errorTitle;
 
+    /// <summary>Message for error alerts. Maximum 255 characters.</summary>
     public string? ErrorMessage { get => _errorMessage; set => _errorMessage = VerifyLength(value, 255); }
     private string? _errorMessage;
 
+    /// <summary>Title for the input message box. Maximum 32 characters.</summary>
     public string? InputTitle { get => _inputTitle; set => _inputTitle = VerifyLength(value, 32); }
     private string? _inputTitle;
 
+    /// <summary>Message for the input message box. Maximum 255 characters.</summary>
     public string? InputMessage { get => _inputMessage; set => _inputMessage = VerifyLength(value, 255); }
     private string? _inputMessage;
 
@@ -49,6 +59,10 @@ public sealed class DataValidation
             ? throw new ArgumentException($"The value can not exceed {maxLength} characters.", nameof(value))
             : value;
 
+    /// <summary>
+    /// Specify how the user is informed about cells with invalid data.
+    /// Defaults to <see cref="ValidationErrorType.Blocking"/>.
+    /// </summary>
     public ValidationErrorType ErrorType
     {
         get => _errorType;
@@ -64,13 +78,22 @@ public sealed class DataValidation
         : new DataValidation(ValidationType.Decimal, op, min.ToStringInvariant(), max.ToStringInvariant());
 
     private static DataValidation Decimal(ValidationOperator op, double value) => new(ValidationType.Decimal, op, value.ToStringInvariant());
+
+    /// <summary>Validate that decimals are between <paramref name="min"/> and <paramref name="max"/>.</summary>
     public static DataValidation DecimalBetween(double min, double max) => Decimal(ValidationOperator.Between, min, max);
+    /// <summary>Validate that decimals are not between <paramref name="min"/> and <paramref name="max"/>.</summary>
     public static DataValidation DecimalNotBetween(double min, double max) => Decimal(ValidationOperator.NotBetween, min, max);
+    /// <summary>Validate that decimals are equal to <paramref name="value"/>.</summary>
     public static DataValidation DecimalEqualTo(double value) => Decimal(ValidationOperator.EqualTo, value);
+    /// <summary>Validate that decimals are not equal to <paramref name="value"/>.</summary>
     public static DataValidation DecimalNotEqualTo(double value) => Decimal(ValidationOperator.NotEqualTo, value);
+    /// <summary>Validate that decimals are greater than <paramref name="value"/>.</summary>
     public static DataValidation DecimalGreaterThan(double value) => Decimal(ValidationOperator.GreaterThan, value);
+    /// <summary>Validate that decimals are greater than or equal to <paramref name="value"/>.</summary>
     public static DataValidation DecimalGreaterThanOrEqualTo(double value) => Decimal(ValidationOperator.GreaterThanOrEqualTo, value);
+    /// <summary>Validate that decimals are less than <paramref name="value"/>.</summary>
     public static DataValidation DecimalLessThan(double value) => Decimal(ValidationOperator.LessThan, value);
+    /// <summary>Validate that decimals are less than or equal to <paramref name="value"/>.</summary>
     public static DataValidation DecimalLessThanOrEqualTo(double value) => Decimal(ValidationOperator.LessThanOrEqualTo, value);
 
     private static DataValidation Integer(ValidationOperator op, int min, int max) => max < min
@@ -78,13 +101,21 @@ public sealed class DataValidation
         : new DataValidation(ValidationType.Integer, op, min.ToStringInvariant(), max.ToStringInvariant());
 
     private static DataValidation Integer(ValidationOperator op, int value) => new(ValidationType.Integer, op, value.ToStringInvariant());
+    /// <summary>Validate that integers are between <paramref name="min"/> and <paramref name="max"/>.</summary>
     public static DataValidation IntegerBetween(int min, int max) => Integer(ValidationOperator.Between, min, max);
+    /// <summary>Validate that integers are not between <paramref name="min"/> and <paramref name="max"/>.</summary>
     public static DataValidation IntegerNotBetween(int min, int max) => Integer(ValidationOperator.NotBetween, min, max);
+    /// <summary>Validate that integers are equal to <paramref name="value"/>.</summary>
     public static DataValidation IntegerEqualTo(int value) => Integer(ValidationOperator.EqualTo, value);
+    /// <summary>Validate that integers are not equal to <paramref name="value"/>.</summary>
     public static DataValidation IntegerNotEqualTo(int value) => Integer(ValidationOperator.NotEqualTo, value);
+    /// <summary>Validate that integers are greater than <paramref name="value"/>.</summary>
     public static DataValidation IntegerGreaterThan(int value) => Integer(ValidationOperator.GreaterThan, value);
+    /// <summary>Validate that integers are greater than or equal to <paramref name="value"/>.</summary>
     public static DataValidation IntegerGreaterThanOrEqualTo(int value) => Integer(ValidationOperator.GreaterThanOrEqualTo, value);
+    /// <summary>Validate that integers are less than <paramref name="value"/>.</summary>
     public static DataValidation IntegerLessThan(int value) => Integer(ValidationOperator.LessThan, value);
+    /// <summary>Validate that integers are less than or equal to <paramref name="value"/>.</summary>
     public static DataValidation IntegerLessThanOrEqualTo(int value) => Integer(ValidationOperator.LessThanOrEqualTo, value);
 
     private static DataValidation TextLength(ValidationOperator op, int min, int max) => max < min
@@ -92,15 +123,24 @@ public sealed class DataValidation
         : new DataValidation(ValidationType.TextLength, op, min.ToStringInvariant(), max.ToStringInvariant());
 
     private static DataValidation TextLength(ValidationOperator op, int value) => new(ValidationType.TextLength, op, value.ToStringInvariant());
+    /// <summary>Validate that text lengths are between <paramref name="min"/> and <paramref name="max"/>.</summary>
     public static DataValidation TextLengthBetween(int min, int max) => TextLength(ValidationOperator.Between, min, max);
+    /// <summary>Validate that text length are not between <paramref name="min"/> and <paramref name="max"/>.</summary>
     public static DataValidation TextLengthNotBetween(int min, int max) => TextLength(ValidationOperator.NotBetween, min, max);
+    /// <summary>Validate that text lengths are equal to <paramref name="value"/>.</summary>
     public static DataValidation TextLengthEqualTo(int value) => TextLength(ValidationOperator.EqualTo, value);
+    /// <summary>Validate that text lengths are not equal to <paramref name="value"/>.</summary>
     public static DataValidation TextLengthNotEqualTo(int value) => TextLength(ValidationOperator.NotEqualTo, value);
+    /// <summary>Validate that text lengths are greater than <paramref name="value"/>.</summary>
     public static DataValidation TextLengthGreaterThan(int value) => TextLength(ValidationOperator.GreaterThan, value);
+    /// <summary>Validate that text lengths are greater than or equal to <paramref name="value"/>.</summary>
     public static DataValidation TextLengthGreaterThanOrEqualTo(int value) => TextLength(ValidationOperator.GreaterThanOrEqualTo, value);
+    /// <summary>Validate that text lengths are less than <paramref name="value"/>.</summary>
     public static DataValidation TextLengthLessThan(int value) => TextLength(ValidationOperator.LessThan, value);
+    /// <summary>Validate that text lengths are less than or equal to <paramref name="value"/>.</summary>
     public static DataValidation TextLengthLessThanOrEqualTo(int value) => TextLength(ValidationOperator.LessThanOrEqualTo, value);
 
+    /// <summary>Validate that cell values equal any of <paramref name="values"/>.</summary>
     public static DataValidation ListValues(IEnumerable<string> values, bool showDropdown = true)
     {
         if (values is null)
