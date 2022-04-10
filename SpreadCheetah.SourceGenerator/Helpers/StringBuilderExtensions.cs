563437a6324b8128a1c4368460185cb3cd4e3bad
sveinungf <ikonage@gmail.com>
2022-04-10 23:43:09 +0200

    Avoid some string allocations in source generator

--- SpreadCheetah.SourceGenerator/Helpers/StringBuilderExtensions.cs
+++ SpreadCheetah.SourceGenerator/Helpers/StringBuilderExtensions.cs
@@ -6,6 +6,18 @@ internal static class StringBuilderExtensions
 {
     public static StringBuilder AppendLine(this StringBuilder sb, int indentationLevel, string value)
     {
-        return sb.Append(new string(' ', 4 * indentationLevel)).AppendLine(value);
+        _ = indentationLevel switch
+        {
+            <= 0 => sb,
+            1 => sb.Append("    "),
+            2 => sb.Append("        "),
+            3 => sb.Append("            "),
+            4 => sb.Append("                "),
+            5 => sb.Append("                    "),
+            6 => sb.Append("                        "),
+            _ => sb.Append(new string(' ', 4 * indentationLevel))
+        };
+
+        return sb.AppendLine(value);
     }
 }
