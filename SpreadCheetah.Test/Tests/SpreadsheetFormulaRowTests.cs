c3b6dfec9ccc6be0888e100c809404a4e9da6c77
sveinungf <ikonage@gmail.com>
2022-07-30 19:46:53 +0200

    More tests for cells with formula

--- SpreadCheetah.Test/Tests/SpreadsheetFormulaRowTests.cs
+++ SpreadCheetah.Test/Tests/SpreadsheetFormulaRowTests.cs
@@ -1,6 +1,7 @@
 using ClosedXML.Excel;
 using SpreadCheetah.Styling;
 using SpreadCheetah.Test.Helpers;
+using System.Drawing;
 using System.Globalization;
 using Xunit;
 
@@ -38,6 +39,76 @@ public class SpreadsheetFormulaRowTests
         var worksheet = workbook.Worksheets.Single();
         var actualCell = worksheet.Cell(1, 1);
         Assert.Equal(formulaText, actualCell.FormulaA1);
+        Assert.Null(actualCell.CachedValue);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task Spreadsheet_AddRow_CellWithFormulaAndStyle(bool bold)
+    {
+        // Arrange
+        const string formulaText = "SUM(A1,A2)";
+        using var stream = new MemoryStream();
+        await using (var spreadsheet = await Spreadsheet.CreateNewAsync(stream))
+        {
+            await spreadsheet.StartWorksheetAsync("Sheet");
+
+            var style = new Style();
+            style.Font.Bold = bold;
+            var styleId = spreadsheet.AddStyle(style);
+
+            var formula = new Formula(formulaText);
+            var cell = new Cell(formula, styleId);
+
+            // Act
+            await spreadsheet.AddRowAsync(cell);
+            await spreadsheet.FinishAsync();
+        }
+
+        // Assert
+        SpreadsheetAssert.Valid(stream);
+        using var workbook = new XLWorkbook(stream);
+        var worksheet = workbook.Worksheets.Single();
+        var actualCell = worksheet.Cell(1, 1);
+        Assert.Equal(formulaText, actualCell.FormulaA1);
+        Assert.Equal(bold, actualCell.Style.Font.Bold);
+        Assert.Null(actualCell.CachedValue);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task Spreadsheet_AddRow_CellWithFormulaAndStyleAndCachedValue(bool italic)
+    {
+        // Arrange
+        const string formulaText = "SUM(A1,A2)";
+        const int cachedValue = int.MinValue;
+        using var stream = new MemoryStream();
+        await using (var spreadsheet = await Spreadsheet.CreateNewAsync(stream))
+        {
+            await spreadsheet.StartWorksheetAsync("Sheet");
+
+            var style = new Style();
+            style.Font.Italic = italic;
+            var styleId = spreadsheet.AddStyle(style);
+
+            var formula = new Formula(formulaText);
+            var cell = new Cell(formula, cachedValue, styleId);
+
+            // Act
+            await spreadsheet.AddRowAsync(cell);
+            await spreadsheet.FinishAsync();
+        }
+
+        // Assert
+        SpreadsheetAssert.Valid(stream);
+        using var workbook = new XLWorkbook(stream);
+        var worksheet = workbook.Worksheets.Single();
+        var actualCell = worksheet.Cell(1, 1);
+        Assert.Equal(formulaText, actualCell.FormulaA1);
+        Assert.Equal(italic, actualCell.Style.Font.Italic);
+        Assert.Equal(cachedValue.ToString(), actualCell.CachedValue);
     }
 
     [Theory]
@@ -71,6 +142,7 @@ public class SpreadsheetFormulaRowTests
         var worksheet = workbook.Worksheets.Single();
         var actualCell = worksheet.Cell(1, 1);
         Assert.Equal(formulaText, actualCell.FormulaA1);
+        Assert.Null(actualCell.CachedValue);
     }
 
     [Theory]
@@ -105,7 +177,42 @@ public class SpreadsheetFormulaRowTests
         var worksheet = workbook.Worksheets.Single();
         var actualCell = worksheet.Cell(1, 1);
         Assert.Equal(formulaText, actualCell.FormulaA1);
-        Assert.Equal(cachedValue, actualCell.Value);
+        Assert.Equal(cachedValue, actualCell.CachedValue);
+    }
+
+    [Theory]
+    [InlineData(100)]
+    [InlineData(511)]
+    [InlineData(512)]
+    [InlineData(513)]
+    [InlineData(4100)]
+    [InlineData(8192)]
+    public async Task Spreadsheet_AddRow_CellWithVeryLongFormulaAndCachedEmptyStringValue(int length)
+    {
+        // Arrange
+        var formulaText = FormulaGenerator.Generate(length);
+        const string cachedValue = "";
+        using var stream = new MemoryStream();
+        var options = new SpreadCheetahOptions { BufferSize = SpreadCheetahOptions.MinimumBufferSize };
+        await using (var spreadsheet = await Spreadsheet.CreateNewAsync(stream, options))
+        {
+            await spreadsheet.StartWorksheetAsync("Sheet");
+
+            var formula = new Formula(formulaText);
+            var cell = new Cell(formula, cachedValue);
+
+            // Act
+            await spreadsheet.AddRowAsync(cell);
+            await spreadsheet.FinishAsync();
+        }
+
+        // Assert
+        SpreadsheetAssert.Valid(stream);
+        using var workbook = new XLWorkbook(stream);
+        var worksheet = workbook.Worksheets.Single();
+        var actualCell = worksheet.Cell(1, 1);
+        Assert.Equal(formulaText, actualCell.FormulaA1);
+        Assert.Equal(cachedValue, actualCell.CachedValue);
     }
 
     [Theory]
@@ -140,23 +247,63 @@ public class SpreadsheetFormulaRowTests
         var worksheet = workbook.Worksheets.Single();
         var actualCell = worksheet.Cell(1, 1);
         Assert.Equal(formulaText, actualCell.FormulaA1);
-        Assert.Equal(cachedValue.ToString(CultureInfo.InvariantCulture), actualCell.Value);
+        Assert.Equal(cachedValue.ToString(CultureInfo.InvariantCulture), actualCell.CachedValue);
     }
 
     [Theory]
-    [InlineData(true)]
-    [InlineData(false)]
-    public async Task Spreadsheet_AddRow_CellWithFormulaAndStyle(bool bold)
+    [InlineData(100, true)]
+    [InlineData(100, false)]
+    [InlineData(100, null)]
+    [InlineData(8192, true)]
+    [InlineData(8192, false)]
+    [InlineData(8192, null)]
+    public async Task Spreadsheet_AddRow_CellWithVeryLongFormulaAndCachedBooleanValue(int length, bool? cachedValue)
     {
         // Arrange
-        const string formulaText = "SUM(A1,A2)";
+        var formulaText = FormulaGenerator.Generate(length);
         using var stream = new MemoryStream();
-        await using (var spreadsheet = await Spreadsheet.CreateNewAsync(stream))
+        var options = new SpreadCheetahOptions { BufferSize = SpreadCheetahOptions.MinimumBufferSize };
+        await using (var spreadsheet = await Spreadsheet.CreateNewAsync(stream, options))
+        {
+            await spreadsheet.StartWorksheetAsync("Sheet");
+
+            var formula = new Formula(formulaText);
+            var cell = new Cell(formula, cachedValue);
+
+            // Act
+            await spreadsheet.AddRowAsync(cell);
+            await spreadsheet.FinishAsync();
+        }
+
+        // Assert
+        SpreadsheetAssert.Valid(stream);
+        using var workbook = new XLWorkbook(stream);
+        var worksheet = workbook.Worksheets.Single();
+        var actualCell = worksheet.Cell(1, 1);
+        Assert.Equal(formulaText, actualCell.FormulaA1);
+        Assert.Equal(cachedValue, actualCell.CachedValue);
+    }
+
+    [Theory]
+    [InlineData(100)]
+    [InlineData(511)]
+    [InlineData(512)]
+    [InlineData(513)]
+    [InlineData(4100)]
+    [InlineData(8192)]
+    public async Task Spreadsheet_AddRow_CellWithVeryLongFormulaAndStyle(int length)
+    {
+        // Arrange
+        var formulaText = FormulaGenerator.Generate(length);
+        var color = Color.Navy;
+        using var stream = new MemoryStream();
+        var options = new SpreadCheetahOptions { BufferSize = SpreadCheetahOptions.MinimumBufferSize };
+        await using (var spreadsheet = await Spreadsheet.CreateNewAsync(stream, options))
         {
             await spreadsheet.StartWorksheetAsync("Sheet");
 
             var style = new Style();
-            style.Font.Bold = bold;
+            style.Fill.Color = color;
             var styleId = spreadsheet.AddStyle(style);
 
             var formula = new Formula(formulaText);
@@ -173,6 +320,49 @@ public class SpreadsheetFormulaRowTests
         var worksheet = workbook.Worksheets.Single();
         var actualCell = worksheet.Cell(1, 1);
         Assert.Equal(formulaText, actualCell.FormulaA1);
-        Assert.Equal(bold, actualCell.Style.Font.Bold);
+        Assert.Equal(color, actualCell.Style.Fill.BackgroundColor.Color);
+        Assert.Null(actualCell.CachedValue);
+    }
+
+    [Theory]
+    [InlineData(100)]
+    [InlineData(511)]
+    [InlineData(512)]
+    [InlineData(513)]
+    [InlineData(4100)]
+    [InlineData(8192)]
+    public async Task Spreadsheet_AddRow_CellWithVeryLongFormulaAndStyleAndCachedValue(int length)
+    {
+        // Arrange
+        var formulaText = FormulaGenerator.Generate(length);
+        const float cachedValue = 5.67f;
+        const string numberFormat = NumberFormats.Percent;
+        using var stream = new MemoryStream();
+        var options = new SpreadCheetahOptions { BufferSize = SpreadCheetahOptions.MinimumBufferSize };
+        await using (var spreadsheet = await Spreadsheet.CreateNewAsync(stream, options))
+        {
+            await spreadsheet.StartWorksheetAsync("Sheet");
+
+            var style = new Style { NumberFormat = numberFormat };
+            var styleId = spreadsheet.AddStyle(style);
+
+            var formula = new Formula(formulaText);
+            var cell = new Cell(formula, cachedValue, styleId);
+
+            // Act
+            await spreadsheet.AddRowAsync(cell);
+            await spreadsheet.FinishAsync();
+        }
+
+        var expectedNumberFormatId = NumberFormats.GetPredefinedNumberFormatId(numberFormat) ?? 0;
+
+        // Assert
+        SpreadsheetAssert.Valid(stream);
+        using var workbook = new XLWorkbook(stream);
+        var worksheet = workbook.Worksheets.Single();
+        var actualCell = worksheet.Cell(1, 1);
+        Assert.Equal(formulaText, actualCell.FormulaA1);
+        Assert.Equal(expectedNumberFormatId, actualCell.Style.NumberFormat.NumberFormatId);
+        Assert.Equal(cachedValue.ToString(CultureInfo.InvariantCulture), actualCell.CachedValue);
     }
 }
