bfeb0a6faf908f4ce602ad5fb736c53bfc74ead3
sveinungf <ikonage@gmail.com>
2022-11-05 13:47:18 +0100

    XML comment on DiagonalBorderType

--- SpreadCheetah/Styling/DiagonalBorderType.cs
+++ SpreadCheetah/Styling/DiagonalBorderType.cs
@@ -1,10 +1,20 @@
 namespace SpreadCheetah.Styling;
 
+/// <summary>
+/// Values that specifies the type of diagonal border used on cells.
+/// </summary>
 [Flags]
 public enum DiagonalBorderType
 {
+    /// <summary>No border.</summary>
     None = 0,
+
+    /// <summary>Diagonal up.</summary>
     DiagonalUp = 1,
+
+    /// <summary>Diagonal down.</summary>
     DiagonalDown = 2,
+
+    /// <summary>Diagonal up and diagonal down.</summary>
     CrissCross = DiagonalUp | DiagonalDown
 }
