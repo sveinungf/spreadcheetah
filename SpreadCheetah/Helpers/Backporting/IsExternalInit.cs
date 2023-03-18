330c0a8d290d187c1efddb303a40c103533da811
sveinungf <ikonage@gmail.com>
2023-03-18 12:38:46 +0100

    Suppress warnings for backports

--- SpreadCheetah/Helpers/Backporting/IsExternalInit.cs
+++ SpreadCheetah/Helpers/Backporting/IsExternalInit.cs
@@ -4,5 +4,7 @@ using System.ComponentModel;
 namespace System.Runtime.CompilerServices;
 
 [EditorBrowsable(EditorBrowsableState.Never)]
+#pragma warning disable CA1812, CA1852 // Sealed and never instantiated
 internal class IsExternalInit { }
+#pragma warning restore CA1812, CA1852 // Sealed and never instantiated
 #endif
