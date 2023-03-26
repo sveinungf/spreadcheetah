68707230610cd1c10054c5ce0246ed9d0f0581c7
sveinungf <ikonage@gmail.com>
2023-03-26 23:40:11 +0200

    Remove the previous Content Types writer implementation

--- SpreadCheetah/MetadataXml/ContentTypesXml.cs
+++ SpreadCheetah/MetadataXml/ContentTypesXml.cs
@@ -5,8 +5,35 @@ using System.Runtime.CompilerServices;
 
 namespace SpreadCheetah.MetadataXml;
 
-internal struct ContentTypesXmlWriter
+internal struct ContentTypesXml
 {
+    public static async ValueTask WriteAsync(
+        ZipArchive archive,
+        CompressionLevel compressionLevel,
+        SpreadsheetBuffer buffer,
+        List<WorksheetMetadata> worksheets,
+        bool hasStylesXml,
+        CancellationToken token)
+    {
+        var stream = archive.CreateEntry("[Content_Types].xml", compressionLevel).Open();
+#if NETSTANDARD2_0
+        using (stream)
+#else
+        await using (stream.ConfigureAwait(false))
+#endif
+        {
+            var writer = new ContentTypesXml(worksheets, hasStylesXml);
+            var done = false;
+
+            do
+            {
+                done = writer.TryWrite(buffer.GetSpan(), out var bytesWritten);
+                buffer.Advance(bytesWritten);
+                await buffer.FlushToStreamAsync(stream, token).ConfigureAwait(false);
+            } while (!done);
+        }
+    }
+
     private static ReadOnlySpan<byte> Header =>
         "<?xml version=\"1.0\" encoding=\"utf-8\"?>"u8 +
         "<Types xmlns=\"http://schemas.openxmlformats.org/package/2006/content-types\">"u8 +
@@ -23,7 +50,7 @@ internal struct ContentTypesXmlWriter
     private Element _nextElement;
     private int _nextWorksheetIndex;
 
-    public ContentTypesXmlWriter(List<WorksheetMetadata> worksheets, bool hasStylesXml)
+    public ContentTypesXml(List<WorksheetMetadata> worksheets, bool hasStylesXml)
     {
         _worksheets = worksheets;
         _hasStylesXml = hasStylesXml;
@@ -95,101 +122,5 @@ internal struct ContentTypesXmlWriter
         Worksheets,
         Footer,
         Done
-    }
-}
-
-internal static class ContentTypesXml
-{
-    private const string Header =
-        "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
-        "<Types xmlns=\"http://schemas.openxmlformats.org/package/2006/content-types\">" +
-        "<Default Extension=\"xml\" ContentType=\"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml\" />" +
-        "<Default Extension=\"rels\" ContentType=\"application/vnd.openxmlformats-package.relationships+xml\" />";
-
-    private const string Styles = "<Override PartName=\"/xl/styles.xml\" ContentType=\"application/vnd.openxmlformats-officedocument.spreadsheetml.styles+xml\" />";
-
-    private const string SheetStartString = "<Override PartName=\"/";
-    private const string SheetEndString = "\" ContentType=\"application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml\" />";
-    private const string Footer = "</Types>";
-
-    private static readonly byte[] SheetStart = Utf8Helper.GetBytes(SheetStartString);
-    private static readonly byte[] SheetEnd = Utf8Helper.GetBytes(SheetEndString);
-
-    public static async ValueTask WriteAsync(
-        ZipArchive archive,
-        CompressionLevel compressionLevel,
-        SpreadsheetBuffer buffer,
-        List<WorksheetMetadata> worksheets,
-        bool hasStylesXml,
-        CancellationToken token)
-    {
-        var stream = archive.CreateEntry("[Content_Types].xml", compressionLevel).Open();
-#if NETSTANDARD2_0
-        using (stream)
-#else
-        await using (stream.ConfigureAwait(false))
-#endif
-        {
-            var writer = new ContentTypesXmlWriter(worksheets, hasStylesXml);
-            var done = false;
-
-            do
-            {
-                done = writer.TryWrite(buffer.GetSpan(), out var bytesWritten);
-                buffer.Advance(bytesWritten);
-                await buffer.FlushToStreamAsync(stream, token).ConfigureAwait(false);
-            } while (!done);
-        }
-    }
-
-    public static async ValueTask WriteAsyncOld(
-        ZipArchive archive,
-        CompressionLevel compressionLevel,
-        SpreadsheetBuffer buffer,
-        List<WorksheetMetadata> worksheets,
-        bool hasStylesXml,
-        CancellationToken token)
-    {
-        var stream = archive.CreateEntry("[Content_Types].xml", compressionLevel).Open();
-#if NETSTANDARD2_0
-        using (stream)
-#else
-        await using (stream.ConfigureAwait(false))
-#endif
-        {
-            buffer.Advance(Utf8Helper.GetBytes(Header, buffer.GetSpan()));
-
-            if (hasStylesXml)
-                await buffer.WriteAsciiStringAsync(Styles, stream, token).ConfigureAwait(false);
-
-            for (var i = 0; i < worksheets.Count; ++i)
-            {
-                var path = worksheets[i].Path;
-                var sheetElementLength = GetSheetElementByteCount(path);
-
-                if (sheetElementLength > buffer.FreeCapacity)
-                    await buffer.FlushToStreamAsync(stream, token).ConfigureAwait(false);
-
-                buffer.Advance(GetSheetElementBytes(path, buffer.GetSpan()));
-            }
-
-            await buffer.WriteAsciiStringAsync(Footer, stream, token).ConfigureAwait(false);
-            await buffer.FlushToStreamAsync(stream, token).ConfigureAwait(false);
-        }
-    }
-
-    private static int GetSheetElementByteCount(string path)
-    {
-        return SheetStart.Length
-            + Utf8Helper.GetByteCount(path)
-            + SheetEnd.Length;
-    }
-
-    private static int GetSheetElementBytes(string path, Span<byte> bytes)
-    {
-        var bytesWritten = SpanHelper.GetBytes(SheetStart, bytes);
-        bytesWritten += Utf8Helper.GetBytes(path, bytes.Slice(bytesWritten));
-        bytesWritten += SpanHelper.GetBytes(SheetEnd, bytes.Slice(bytesWritten));
-        return bytesWritten;
     }
 }
