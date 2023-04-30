806584af176de537e365ad4c2c40976d3b610110
sveinungf <ikonage@gmail.com>
2023-04-30 17:06:38 +0200

    Write styles.xml styles with new writer pattern

--- SpreadCheetah/MetadataXml/StylesXml.cs
+++ SpreadCheetah/MetadataXml/StylesXml.cs
@@ -3,7 +3,6 @@ using SpreadCheetah.Styling;
 using SpreadCheetah.Styling.Internal;
 using System.Collections.Immutable;
 using System.IO.Compression;
-using System.Text;
 
 namespace SpreadCheetah.MetadataXml;
 
@@ -31,7 +30,7 @@ internal struct StylesXml
         await using (stream.ConfigureAwait(false))
 #endif
         {
-            var writer = new StylesXml(styles);
+            var writer = new StylesXml(styles.Keys.ToList());
             var done = false;
 
             do
@@ -49,19 +48,21 @@ internal struct StylesXml
         """<?xml version="1.0" encoding="utf-8"?>"""u8 +
         """<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">"""u8;
 
-    private readonly Dictionary<ImmutableStyle, int> _styles;
+    private readonly List<ImmutableStyle> _styles;
     private readonly StyleNumberFormatsXml _numberFormatsXml;
     private readonly StyleBordersXml _bordersXml;
     private readonly StyleFillsXml _fillsXml;
     private readonly StyleFontsXml _fontsXml;
     private Element _next;
+    private int _nextIndex;
 
+    // TODO: Make these fields
     public Dictionary<string, int>? CustomNumberFormats { get; }
     public Dictionary<ImmutableBorder, int> Borders { get; }
     public Dictionary<ImmutableFill, int> Fills { get; }
     public Dictionary<ImmutableFont, int> Fonts { get; }
 
-    private StylesXml(Dictionary<ImmutableStyle, int> styles)
+    private StylesXml(List<ImmutableStyle> styles)
     {
         CustomNumberFormats = CreateCustomNumberFormatDictionary(styles);
         Borders = CreateBorderDictionary(styles);
@@ -74,12 +75,12 @@ internal struct StylesXml
         _styles = styles;
     }
 
-    private static Dictionary<string, int>? CreateCustomNumberFormatDictionary(Dictionary<ImmutableStyle, int> styles)
+    private static Dictionary<string, int>? CreateCustomNumberFormatDictionary(List<ImmutableStyle> styles)
     {
         Dictionary<string, int>? dictionary = null;
         var numberFormatId = 165; // Custom formats start sequentially from this ID
 
-        foreach (var style in styles.Keys)
+        foreach (var style in styles)
         {
             var numberFormat = style.NumberFormat;
             if (numberFormat is null) continue;
@@ -93,7 +94,7 @@ internal struct StylesXml
         return dictionary;
     }
 
-    private static Dictionary<ImmutableBorder, int> CreateBorderDictionary(Dictionary<ImmutableStyle, int> styles)
+    private static Dictionary<ImmutableBorder, int> CreateBorderDictionary(List<ImmutableStyle> styles)
     {
         var defaultBorder = new ImmutableBorder();
         const int defaultCount = 1;
@@ -101,7 +102,7 @@ internal struct StylesXml
         var uniqueBorders = new Dictionary<ImmutableBorder, int> { { defaultBorder, 0 } };
         var borderIndex = defaultCount;
 
-        foreach (var style in styles.Keys)
+        foreach (var style in styles)
         {
             var border = style.Border;
             if (!uniqueBorders.ContainsKey(border)) // TODO: Use CollectionsMarshal
@@ -114,7 +115,7 @@ internal struct StylesXml
         return uniqueBorders;
     }
 
-    private static Dictionary<ImmutableFill, int> CreateFillDictionary(Dictionary<ImmutableStyle, int> styles)
+    private static Dictionary<ImmutableFill, int> CreateFillDictionary(List<ImmutableStyle> styles)
     {
         var defaultFill = new ImmutableFill();
         const int defaultCount = 2;
@@ -122,7 +123,7 @@ internal struct StylesXml
         var uniqueFills = new Dictionary<ImmutableFill, int> { { defaultFill, 0 } };
         var fillIndex = defaultCount;
 
-        foreach (var style in styles.Keys)
+        foreach (var style in styles)
         {
             var fill = style.Fill;
             if (!uniqueFills.ContainsKey(fill)) // TODO: Use CollectionsMarshal
@@ -135,7 +136,7 @@ internal struct StylesXml
         return uniqueFills;
     }
 
-    private static Dictionary<ImmutableFont, int> CreateFontDictionary(Dictionary<ImmutableStyle, int> styles)
+    private static Dictionary<ImmutableFont, int> CreateFontDictionary(List<ImmutableStyle> styles)
     {
         var defaultFont = new ImmutableFont { Size = Font.DefaultSize };
         const int defaultCount = 1;
@@ -143,7 +144,7 @@ internal struct StylesXml
         var uniqueFonts = new Dictionary<ImmutableFont, int> { { defaultFont, 0 } };
         var fontIndex = defaultCount;
 
-        foreach (var style in styles.Keys)
+        foreach (var style in styles)
         {
             var font = style.Font;
             if (!uniqueFonts.ContainsKey(font)) // TODO: Use CollectionsMarshal
@@ -166,6 +167,7 @@ internal struct StylesXml
         if (_next == Element.Fills && !Advance(_fillsXml.TryWrite(bytes, ref bytesWritten))) return false;
         if (_next == Element.Borders && !Advance(_bordersXml.TryWrite(bytes, ref bytesWritten))) return false;
         if (_next == Element.CellXfsStart && !Advance(TryWriteCellXfsStart(bytes, ref bytesWritten))) return false;
+        if (_next == Element.Styles && !Advance(TryWriteStyles(bytes, ref bytesWritten))) return false;
 
         return true;
     }
@@ -195,6 +197,62 @@ internal struct StylesXml
         return true;
     }
 
+    private bool TryWriteStyles(Span<byte> bytes, ref int bytesWritten)
+    {
+        var styles = _styles;
+
+        for (; _nextIndex < styles.Count; ++_nextIndex)
+        {
+            var style = _styles[_nextIndex];
+            var span = bytes.Slice(bytesWritten);
+            var written = 0;
+
+            var numberFormatId = GetNumberFormatId(style.NumberFormat, CustomNumberFormats);
+
+            if (!"<xf numFmtId=\""u8.TryCopyTo(span, ref written)) return false;
+            if (!SpanHelper.TryWrite(numberFormatId, span, ref written)) return false;
+            if (!"\""u8.TryCopyTo(span, ref written)) return false;
+            if (numberFormatId > 0 && !" applyNumberFormat=\"1\""u8.TryCopyTo(span, ref written)) return false;
+
+            var fontIndex = Fonts.GetValueOrDefault(style.Font);
+            if (!" fontId=\""u8.TryCopyTo(span, ref written)) return false;
+            if (!SpanHelper.TryWrite(fontIndex, span, ref written)) return false;
+            if (!"\""u8.TryCopyTo(span, ref written)) return false;
+            if (fontIndex > 0 && !" applyFont=\"1\""u8.TryCopyTo(span, ref written)) return false;
+
+            var fillIndex = Fills.GetValueOrDefault(style.Fill);
+            if (!" fillId=\""u8.TryCopyTo(span, ref written)) return false;
+            if (!SpanHelper.TryWrite(fillIndex, span, ref written)) return false;
+            if (!"\""u8.TryCopyTo(span, ref written)) return false;
+            if (fillIndex > 0 && !" applyFill=\"1\""u8.TryCopyTo(span, ref written)) return false;
+
+            if (Borders.TryGetValue(style.Border, out var borderIndex) && borderIndex > 0)
+            {
+                if (!" borderId=\""u8.TryCopyTo(span, ref written)) return false;
+                if (!SpanHelper.TryWrite(borderIndex, span, ref written)) return false;
+                if (!"\" applyBorder=\"1\""u8.TryCopyTo(span, ref written)) return false;
+            }
+
+            if (!" xfId=\"0\""u8.TryCopyTo(span, ref written)) return false;
+
+            var defaultAlignment = new ImmutableAlignment();
+            if (style.Alignment == defaultAlignment)
+            {
+                if (!"/>"u8.TryCopyTo(span, ref written)) return false;
+            }
+            else
+            {
+                if (!""" applyAlignment="1">"""u8.TryCopyTo(span, ref written)) return false;
+                if (!TryWriteAlignment(style.Alignment, span, ref written)) return false;
+                if (!"</xf>"u8.TryCopyTo(span, ref written)) return false;
+            }
+
+            bytesWritten += written;
+        }
+
+        return true;
+    }
+
     private static async ValueTask WriteAsync(
         Stream stream,
         SpreadsheetBuffer buffer,
@@ -205,53 +263,8 @@ internal struct StylesXml
         Dictionary<ImmutableFont, int> fontLookup,
         CancellationToken token)
     {
-        var sb = new StringBuilder();
-
-        // Assuming here that the built-in default style is part of 'styles'
-        foreach (var style in styles.Keys)
-        {
-            AppendStyle(sb, style, customNumberFormatLookup, fontLookup, fillLookup, borderLookup);
-            await buffer.WriteAsciiStringAsync(sb.ToString(), stream, token).ConfigureAwait(false);
-            sb.Clear();
-        }
-
         await buffer.WriteAsciiStringAsync(XmlPart2, stream, token).ConfigureAwait(false);
         await buffer.FlushToStreamAsync(stream, token).ConfigureAwait(false);
-    }
-
-    private static void AppendStyle(StringBuilder sb,
-        in ImmutableStyle style,
-        IReadOnlyDictionary<string, int>? customNumberFormats,
-        IReadOnlyDictionary<ImmutableFont, int> fonts,
-        IReadOnlyDictionary<ImmutableFill, int> fills,
-        IReadOnlyDictionary<ImmutableBorder, int> borders)
-    {
-        var numberFormatId = GetNumberFormatId(style.NumberFormat, customNumberFormats);
-        sb.Append("<xf numFmtId=\"").Append(numberFormatId).Append('"');
-        if (numberFormatId > 0) sb.Append(" applyNumberFormat=\"1\"");
-
-        var fontIndex = fonts[style.Font];
-        sb.Append(" fontId=\"").Append(fontIndex).Append('"');
-        if (fontIndex > 0) sb.Append(" applyFont=\"1\"");
-
-        var fillIndex = fills[style.Fill];
-        sb.Append(" fillId=\"").Append(fillIndex).Append('"');
-        if (fillIndex > 1) sb.Append(" applyFill=\"1\"");
-
-        if (borders.TryGetValue(style.Border, out var borderIndex) && borderIndex > 0)
-            sb.Append(" borderId=\"").Append(borderIndex).Append("\" applyBorder=\"1\"");
-
-        sb.Append(" xfId=\"0\"");
-
-        var defaultAlignment = new ImmutableAlignment();
-        if (style.Alignment == defaultAlignment)
-            sb.Append("/>");
-        else
-        {
-            sb.Append(" applyAlignment=\"1\">");
-            AppendAlignment(sb, style.Alignment);
-            sb.Append("</xf>");
-        }
     }
 
     private static int GetNumberFormatId(string? numberFormat, IReadOnlyDictionary<string, int>? customNumberFormats)
@@ -262,34 +275,42 @@ internal struct StylesXml
             ?? 0;
     }
 
-    private static StringBuilder AppendAlignment(StringBuilder sb, ImmutableAlignment alignment)
+    private static bool TryWriteAlignment(ImmutableAlignment alignment, Span<byte> bytes, ref int bytesWritten)
     {
-        sb.Append("<alignment");
-        AppendHorizontalAlignment(sb, alignment.Horizontal);
-        AppendVerticalAlignment(sb, alignment.Vertical);
+        var span = bytes.Slice(bytesWritten);
+        var written = 0;
 
-        if (alignment.WrapText)
-            sb.Append(" wrapText=\"1\"");
+        if (!"<alignment"u8.TryCopyTo(span, ref written)) return false;
+        if (!TryWriteHorizontalAlignment(alignment.Horizontal, span, ref written)) return false;
+        if (!TryWriteVerticalAlignment(alignment.Vertical, span, ref written)) return false;
+        if (alignment.WrapText && !" wrapText=\"1\""u8.TryCopyTo(span, ref written)) return false;
 
         if (alignment.Indent > 0)
-            sb.Append(" indent=\"").Append(alignment.Indent).Append('"');
+        {
+            if (!" indent=\""u8.TryCopyTo(span, ref written)) return false;
+            if (!SpanHelper.TryWrite(alignment.Indent, span, ref written)) return false;
+            if (!"\""u8.TryCopyTo(span, ref written)) return false;
+        }
 
-        return sb.Append("/>");
+        if (!"/>"u8.TryCopyTo(span, ref written)) return false;
+
+        bytesWritten += written;
+        return true;
     }
 
-    private static StringBuilder AppendHorizontalAlignment(StringBuilder sb, HorizontalAlignment alignment) => alignment switch
+    private static bool TryWriteHorizontalAlignment(HorizontalAlignment alignment, Span<byte> bytes, ref int bytesWritten) => alignment switch
     {
-        HorizontalAlignment.Left => sb.Append(" horizontal=\"left\""),
-        HorizontalAlignment.Center => sb.Append(" horizontal=\"center\""),
-        HorizontalAlignment.Right => sb.Append(" horizontal=\"right\""),
-        _ => sb
+        HorizontalAlignment.Left => " horizontal=\"left\""u8.TryCopyTo(bytes, ref bytesWritten),
+        HorizontalAlignment.Center => " horizontal=\"center\""u8.TryCopyTo(bytes, ref bytesWritten),
+        HorizontalAlignment.Right => " horizontal=\"right\""u8.TryCopyTo(bytes, ref bytesWritten),
+        _ => true
     };
 
-    private static StringBuilder AppendVerticalAlignment(StringBuilder sb, VerticalAlignment alignment) => alignment switch
+    private static bool TryWriteVerticalAlignment(VerticalAlignment alignment, Span<byte> bytes, ref int bytesWritten) => alignment switch
     {
-        VerticalAlignment.Center => sb.Append(" vertical=\"center\""),
-        VerticalAlignment.Top => sb.Append(" vertical=\"top\""),
-        _ => sb
+        VerticalAlignment.Center => " vertical=\"center\""u8.TryCopyTo(bytes, ref bytesWritten),
+        VerticalAlignment.Top => " vertical=\"top\""u8.TryCopyTo(bytes, ref bytesWritten),
+        _ => true
     };
 
     private enum Element
@@ -300,6 +321,7 @@ internal struct StylesXml
         Fills,
         Borders,
         CellXfsStart,
+        Styles,
         Done
     }
 }
