2c38cfa7ca67967669540df773bec555d1ff873b
sveinungf <ikonage@gmail.com>
2023-06-04 22:56:46 +0200

    Test explicit cell reference for multiple worksheets

--- SpreadCheetah.Test/Tests/SpreadsheetRowTests.cs
+++ SpreadCheetah.Test/Tests/SpreadsheetRowTests.cs
@@ -542,36 +542,48 @@ public class SpreadsheetRowTests
         // Arrange
         using var stream = new MemoryStream();
         await using var spreadsheet = await Spreadsheet.CreateNewAsync(stream);
-        await spreadsheet.StartWorksheetAsync("Sheet", new WorksheetOptions { WriteCellReferenceAttributes = true });
 
-        var row1 = Enumerable.Range(1, 1).Select(_ => CellFactory.Create(cellType, valueType, isNull, null, out var value)).ToList();
-        var row2 = Enumerable.Range(1, 10).Select(_ => CellFactory.Create(cellType, valueType, isNull, null, out var value)).ToList();
+        var row1 = Enumerable.Range(1, 10).Select(_ => CellFactory.Create(cellType, valueType, isNull, null, out var value)).ToList();
+        var row2 = Enumerable.Range(1, 1).Select(_ => CellFactory.Create(cellType, valueType, isNull, null, out var value)).ToList();
         var row3 = Enumerable.Range(1, 100).Select(_ => CellFactory.Create(cellType, valueType, isNull, null, out var value)).ToList();
 
-        var expectedRow1Refs = Enumerable.Range(1, 1).Select(x => SpreadsheetUtility.GetColumnName(x) + "1").OfType<string?>();
-        var expectedRow2Refs = Enumerable.Range(1, 10).Select(x => SpreadsheetUtility.GetColumnName(x) + "2").OfType<string?>();
+        var expectedRow1Refs = Enumerable.Range(1, 10).Select(x => SpreadsheetUtility.GetColumnName(x) + "1").OfType<string?>();
+        var expectedRow2Refs = Enumerable.Range(1, 1).Select(x => SpreadsheetUtility.GetColumnName(x) + "2").OfType<string?>();
         var expectedRow3Refs = Enumerable.Range(1, 100).Select(x => SpreadsheetUtility.GetColumnName(x) + "3").OfType<string?>();
 
+        var options = new WorksheetOptions { WriteCellReferenceAttributes = true };
+
         // Act
+        await spreadsheet.StartWorksheetAsync("Sheet1", options);
         await spreadsheet.AddRowAsync(row1, rowType);
         await spreadsheet.AddRowAsync(row2, rowType);
         await spreadsheet.AddRowAsync(row3, rowType);
+
+        await spreadsheet.StartWorksheetAsync("Sheet2", options);
+        await spreadsheet.AddRowAsync(row1, rowType);
+
         await spreadsheet.FinishAsync();
 
         // Assert
         SpreadsheetAssert.Valid(stream);
         using var actual = SpreadsheetDocument.Open(stream, true);
-        var sheetPart = actual.WorkbookPart!.WorksheetParts.Single();
-        var rows = sheetPart.Worksheet.Descendants<Row>().ToList();
-        Assert.Equal(3, rows.Count);
+        var sheetParts = actual.WorkbookPart!.WorksheetParts.ToList();
+        Assert.Equal(2, sheetParts.Count);
 
-        var actualRow1Refs = rows[0].Descendants<OpenXmlCell>().Select(x => x.CellReference?.Value);
-        var actualRow2Refs = rows[1].Descendants<OpenXmlCell>().Select(x => x.CellReference?.Value);
-        var actualRow3Refs = rows[2].Descendants<OpenXmlCell>().Select(x => x.CellReference?.Value);
+        var sheet1Rows = sheetParts[0].Worksheet.Descendants<Row>().ToList();
+        Assert.Equal(3, sheet1Rows.Count);
+
+        var actualRow1Refs = sheet1Rows[0].Descendants<OpenXmlCell>().Select(x => x.CellReference?.Value);
+        var actualRow2Refs = sheet1Rows[1].Descendants<OpenXmlCell>().Select(x => x.CellReference?.Value);
+        var actualRow3Refs = sheet1Rows[2].Descendants<OpenXmlCell>().Select(x => x.CellReference?.Value);
 
         Assert.Equal(expectedRow1Refs, actualRow1Refs);
         Assert.Equal(expectedRow2Refs, actualRow2Refs);
         Assert.Equal(expectedRow3Refs, actualRow3Refs);
+
+        var sheet2Row = sheetParts[1].Worksheet.Descendants<Row>().Single();
+        var actualSheet2Refs = sheet2Row.Descendants<OpenXmlCell>().Select(x => x.CellReference?.Value);
+        Assert.Equal(expectedRow1Refs, actualSheet2Refs);
     }
 
     public static IEnumerable<object?[]> RowHeights() => TestData.CombineWithCellTypes<double?>(
