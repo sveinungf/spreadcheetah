5c2e1f764edb4e41969e27fd4b22a1c5be211c4e
sveinungf <ikonage@gmail.com>
2023-07-15 00:26:11 +0200

    Replace polyfills with the Polyfill package

--- SpreadCheetah/Helpers/Backporting/DoesNotReturnAttribute.cs
+++
@@ -1,6 +0,0 @@
-#if NETSTANDARD2_0
-namespace System.Diagnostics.CodeAnalysis;
-
-[AttributeUsage(AttributeTargets.Method, Inherited = false)]
-internal sealed class DoesNotReturnAttribute : Attribute { }
-#endif
--- SpreadCheetah/Helpers/Backporting/StringExtensions.cs
+++ SpreadCheetah/Helpers/Backporting/StringExtensions.cs
@@ -9,15 +9,5 @@ internal static class StringExtensions
     {
         return value.IndexOf(character) >= 0;
     }
-
-    public static bool StartsWith(this string value, char character)
-    {
-        return value.Length > 0 && value[0] == character;
-    }
-
-    public static bool EndsWith(this string value, char character)
-    {
-        return value.Length > 0 && value[value.Length - 1] == character;
-    }
 }
 #endif
