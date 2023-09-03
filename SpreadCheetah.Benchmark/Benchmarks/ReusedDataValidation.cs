a82d14da2968d25f41be266f811f6ed8d7378114
sveinungf <ikonage@gmail.com>
2023-09-03 15:39:19 +0200

    Benchmark for data validations

---
+++ SpreadCheetah.Benchmark/Benchmarks/ReusedDataValidation.cs
@@ -0,0 +1,27 @@
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using SpreadCheetah.Validations;
+
+namespace SpreadCheetah.Benchmark.Benchmarks;
+
+[SimpleJob(RuntimeMoniker.Net70)]
+[MemoryDiagnoser]
+public class ReusedDataValidation
+{
+    [Benchmark]
+    public async Task SpreadCheetah()
+    {
+        var options = new SpreadCheetahOptions { DefaultDateTimeFormat = null };
+        await using var spreadsheet = await Spreadsheet.CreateNewAsync(Stream.Null, options);
+        await spreadsheet.StartWorksheetAsync("Book1");
+        var validation = DataValidation.IntegerGreaterThan(0);
+
+        for (var i = 1; i <= 65534; ++i)
+        {
+            var reference = "A" + i;
+            spreadsheet.TryAddDataValidation(reference, validation);
+        }
+
+        await spreadsheet.FinishAsync();
+    }
+}
