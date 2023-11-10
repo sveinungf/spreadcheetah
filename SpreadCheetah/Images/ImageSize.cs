0250d815dc51e3a5e42bf730563750d3ffcd8097
sveinungf <ikonage@gmail.com>
2023-11-10 18:11:09 +0100

    FillCell options on ImageSize

--- SpreadCheetah/Images/ImageSize.cs
+++ SpreadCheetah/Images/ImageSize.cs
@@ -1,11 +1,16 @@
+using SpreadCheetah.CellReferences;
 using SpreadCheetah.Helpers;
 
 namespace SpreadCheetah.Images;
 
 public sealed class ImageSize
 {
+    private static ImageSize FillCellInstance { get; } = new() { FillCellValue = true };
+
     internal decimal? ScaleValue { get; private set; }
     internal (int Width, int Height)? DimensionsValue { get; private set; }
+    internal bool FillCellValue { get; private set; }
+    internal SingleCellRelativeReference? FillCellRangeLowerRightReference { get; private set; }
 
     private ImageSize()
     {
@@ -29,12 +34,16 @@ public sealed class ImageSize
     }
 
     // TODO: Fill cell option: A way of doing this could be to pass two cell references when adding images. Can fill multiple cells this way.
+    public static ImageSize FillCell() => FillCellInstance;
 
-    // TODO: Fit cell option: Keep aspect ratio, but with maximum image size inside a cell (or multiple cells).
-    // TODO: Should probably pass two cell references for this option as well, otherwise resizing with cells won't work.
-    // TODO: Should have some alignment option as well? But that won't work with offsets.
-    // TODO: Will need to know column widths and row heights. Column widths should be known. Total row height should be passed as argument? Note: Can be for multiple columns/rows.
-    // TODO: Perhaps this option can come in a later version.
+    public static ImageSize FillCellRange(string lowerRightCellReference)
+    {
+        return new ImageSize
+        {
+            FillCellValue = true,
+            FillCellRangeLowerRightReference = SingleCellRelativeReference.Create(lowerRightCellReference)
+        };
+    }
 
     // TODO: Different image options when adding for a single cell reference compared to for two cell references?
 }
