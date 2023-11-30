e47bf51e03c90f8ac74811d6b4930bbea7a67d3c
sveinungf <ikonage@gmail.com>
2023-11-30 23:19:29 +0100

    Console app for testing Native AOT compatibility

--- SpreadCheetah.AotCompatibilityCheck/Program.cs
+++ SpreadCheetah.AotCompatibilityCheck/Program.cs
@@ -1,2 +1,2 @@
-Console.WriteLine("Publish this project with 'dotnet publish -c Release -r win-x64' to show trim warnings.");
+Console.WriteLine("Publish this project with 'dotnet publish -c Release -r win-x64' to show trim/AOT warnings.");
 Console.WriteLine("See https://docs.microsoft.com/en-us/dotnet/core/deploying/trimming/prepare-libraries-for-trimming for more information.");
