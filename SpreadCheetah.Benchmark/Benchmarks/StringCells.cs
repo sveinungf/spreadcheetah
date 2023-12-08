1a4157ce92dfb9186bd4a6650e8891cf2dc1af27
sveinungf <ikonage@gmail.com>
2023-12-08 11:29:16 +0100

    Run StringCells benchmark on .NET 8

--- SpreadCheetah.Benchmark/Benchmarks/StringCells.cs
+++ SpreadCheetah.Benchmark/Benchmarks/StringCells.cs
@@ -12,7 +12,7 @@ namespace SpreadCheetah.Benchmark.Benchmarks;
 
 [SimpleJob(RuntimeMoniker.Net48)]
 [SimpleJob(RuntimeMoniker.Net60)]
-[SimpleJob(RuntimeMoniker.Net70)]
+[SimpleJob(RuntimeMoniker.Net80)]
 [MemoryDiagnoser]
 public class StringCells : IDisposable
 {
