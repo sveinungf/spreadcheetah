78b9f8356faa538c6d54fef16e15087c3477c4d4
sveinungf <ikonage@gmail.com>
2023-12-29 18:37:36 +0100

    Snapshot test for class with duplicate column ordering

--- SpreadCheetah.SourceGenerator.SnapshotTest/Tests/WorksheetRowGeneratorColumnOrderTests.cs
+++ SpreadCheetah.SourceGenerator.SnapshotTest/Tests/WorksheetRowGeneratorColumnOrderTests.cs
@@ -15,12 +15,39 @@ public class WorksheetRowGeneratorColumnOrderTests
             using SpreadCheetah.SourceGenerator.SnapshotTest.Models.ColumnOrdering;
             using System;
 
-            namespace MyNamespace
+            namespace MyNamespace;
+            
+            [WorksheetRow(typeof(ClassWithColumnOrderForAllProperties))]
+            public partial class MyGenRowContext : WorksheetRowContext
             {
-                [WorksheetRow(typeof(ClassWithColumnOrderForAllProperties))]
-                public partial class MyGenRowContext : WorksheetRowContext
-                {
-                }
+            }
+            """;
+
+        // Act & Assert
+        return TestHelper.CompileAndVerify<WorksheetRowGenerator>(source);
+    }
+
+    [Fact]
+    public Task WorksheetRowGenerator_Generate_ClassWithDuplicateColumnOrdering()
+    {
+        // Arrange
+        const string source = """
+            using SpreadCheetah.SourceGeneration;
+            using System;
+
+            namespace MyNamespace;
+
+            public class ClassWithDuplicateColumnOrdering
+            {
+                [ColumnOrder(1)]
+                public string PropertyA { get; set; }
+                [ColumnOrder(1)]
+                public string PropertyB { get; set; }
+            }
+            
+            [WorksheetRow(typeof(ClassWithDuplicateColumnOrdering))]
+            public partial class MyGenRowContext : WorksheetRowContext
+            {
             }
             """;
 
