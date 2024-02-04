679fcee2261709db7032a221ed5e5e32e96335db
sveinungf <ikonage@gmail.com>
2024-02-04 14:36:53 +0100

    Look for ColumnHeaderAttribute in source generator

--- SpreadCheetah.SourceGenerator/Helpers/TypePropertiesInfo.cs
+++ SpreadCheetah.SourceGenerator/Helpers/TypePropertiesInfo.cs
@@ -1,7 +1,7 @@
 using Microsoft.CodeAnalysis;
+using SpreadCheetah.SourceGenerator.Models;
 
 namespace SpreadCheetah.SourceGenerator.Helpers;
-
 internal sealed record TypePropertiesInfo(
-    SortedDictionary<int, string> PropertyNames,
+    SortedDictionary<int, ColumnProperty> Properties,
     List<IPropertySymbol> UnsupportedProperties);
---
+++ SpreadCheetah.SourceGenerator/Models/ColumnProperty.cs
@@ -0,0 +1,3 @@
+namespace SpreadCheetah.SourceGenerator.Models;
+
+internal sealed record ColumnProperty(string PropertyName, string ColumnHeader);
