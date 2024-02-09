cab24b0b7bdb63e7a9448906684a0413e6ff926b
sveinungf <ikonage@gmail.com>
2024-02-09 13:58:34 +0100

    Snapshot tests for use of ColumnHeader attributes

---
+++ SpreadCheetah.SourceGenerator.SnapshotTest/Models/ColumnHeader/ClassWithSpecialCharacterColumnHeaders.cs
@@ -0,0 +1,39 @@
+using SpreadCheetah.SourceGeneration;
+
+namespace SpreadCheetah.SourceGenerator.SnapshotTest.Models.ColumnHeader;
+
+public class ClassWithSpecialCharacterColumnHeaders
+{
+    [ColumnHeader("First name")]
+    public string? FirstName { get; set; }
+
+    [ColumnHeader("")]
+    public string? LastName { get; set; }
+
+    [ColumnHeader("Nationality (escaped characters \", \', \\)")]
+    public string? Nationality { get; set; }
+
+    [ColumnHeader("Address line 1 (escaped characters \r\n, \t)")]
+    public string? AddressLine1 { get; set; }
+
+    [ColumnHeader(@"Address line 2 (verbatim
+string: "", \)")]
+    public string? AddressLine2 { get; set; }
+
+    [ColumnHeader("""
+        Age (
+            raw
+            string
+            literal
+        )
+    """)]
+    public int Age { get; set; }
+
+    [ColumnHeader("Note (unicode escape sequence 🌉, \ud83d\udc4d, \xE7)")]
+    public string? Note { get; set; }
+
+    private const string Constant = "This is a constant";
+
+    [ColumnHeader($"Note 2 (constant interpolated string: {Constant})")]
+    public string? Note2 { get; set; }
+}
