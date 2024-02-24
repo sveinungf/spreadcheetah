aa85d893ef7d2a68dc82e8a313ef7d8829bdc9a4
sveinungf <ikonage@gmail.com>
2024-02-25 00:03:56 +0100

    Update XML doc for source generator related code

--- SpreadCheetah/SourceGeneration/ColumnHeaderAttribute.cs
+++ SpreadCheetah/SourceGeneration/ColumnHeaderAttribute.cs
@@ -1,4 +1,9 @@
 namespace SpreadCheetah.SourceGeneration;
 
+/// <summary>
+/// Instructs the SpreadCheetah source generator about what header name to use for a column.
+/// The source generator creates columns from properties, and using the attribute on a property will set the header name for the resulting column.
+/// Header names are written to a worksheet with <see cref="Spreadsheet.AddHeaderRowAsync"/>.
+/// </summary>
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
 public sealed class ColumnHeaderAttribute(string name) : Attribute;
--- SpreadCheetah/SourceGeneration/ColumnOrderAttribute.cs
+++ SpreadCheetah/SourceGeneration/ColumnOrderAttribute.cs
@@ -1,13 +1,15 @@
 namespace SpreadCheetah.SourceGeneration;
 
 /// <summary>
-/// Instructs the SpreadCheetah source generator to specify column ordering.
-/// The order can be customized by using this attribute on a type's properties.
+/// Instructs the SpreadCheetah source generator to specify the order between columns.
+/// The source generator creates columns from properties, and using the attribute on a property will set the order for the resulting column.
 /// <list type="bullet">
-///   <item><description>The order will be in ascending order based on the <c>order</c> parameter. E.g. a property with <c>ColumnOrder(1)</c> will be in the column before the column for property with <c>ColumnOrder(2)</c>.</description></item>
+///   <item><description>The order will be in ascending order based on the <c>order</c> parameter. E.g. the column from property with <c>ColumnOrder(1)</c> will be before
+///   the column from property with <c>ColumnOrder(2)</c>.</description></item>
 ///   <item><description>The value for the <c>order</c> parameter can be any integer, including negative integers.</description></item>
-///   <item><description>Two properties can not have the same value for the <c>order</c> parameter.</description></item>
-///   <item><description>Properties that don't have the attribute will implicitly get the first non-duplicate value for <c>order</c> starting at 1.</description></item>
+///   <item><description>Two different properties on a type can not have the same value for the <c>order</c> parameter.</description></item>
+///   <item><description>The values used for a type's properties don't have to be consecutive.</description></item>
+///   <item><description>Properties that don't have the attribute will implicitly get the first available value for <c>order</c> starting at 1.</description></item>
 /// </list>
 /// </summary>
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
