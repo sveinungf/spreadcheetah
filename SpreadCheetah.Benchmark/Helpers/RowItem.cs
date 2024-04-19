c2d33c538d16ed67ee8104f0276dbc4ffb1697a5
sveinungf <ikonage@gmail.com>
2024-04-19 16:52:14 +0200

    Update the MixedDataTypeCells benchmark

--- SpreadCheetah.Benchmark/Helpers/RowItem.cs
+++ SpreadCheetah.Benchmark/Helpers/RowItem.cs
@@ -1,3 +1,3 @@
 namespace SpreadCheetah.Benchmark.Helpers;
 
-public record RowItem(int A, string B, string C, int D, double E, bool F, string G, bool H, double I, int J);
+public record RowItem(int A, string B, string? C, int? D, double E, bool F, string G, bool H, double? I, int J, DateTime? K);
