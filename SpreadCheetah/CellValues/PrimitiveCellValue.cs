056ff61a405a183b9ee7c2b2b9c90eb87ce8deda
sveinungf <ikonage@gmail.com>
2024-04-19 22:59:09 +0200

    Simplify PrimitiveCellValue

--- SpreadCheetah/CellValues/PrimitiveCellValue.cs
+++ SpreadCheetah/CellValues/PrimitiveCellValue.cs
@@ -9,27 +9,14 @@ internal readonly struct PrimitiveCellValue
     [FieldOffset(0)] public readonly float FloatValue;
     [FieldOffset(0)] public readonly double DoubleValue;
 
+    public PrimitiveCellValue()
+    {
 #if NET5_0_OR_GREATER
-    public PrimitiveCellValue(int value)
-    {
         System.Runtime.CompilerServices.Unsafe.SkipInit(out this);
-        IntValue = value;
+#endif
     }
 
-    public PrimitiveCellValue(float value)
-    {
-        System.Runtime.CompilerServices.Unsafe.SkipInit(out this);
-        FloatValue = value;
-    }
-
-    public PrimitiveCellValue(double value)
-    {
-        System.Runtime.CompilerServices.Unsafe.SkipInit(out this);
-        DoubleValue = value;
-    }
-#else
     public PrimitiveCellValue(int value) : this() => IntValue = value;
     public PrimitiveCellValue(float value) : this() => FloatValue = value;
     public PrimitiveCellValue(double value) : this() => DoubleValue = value;
-#endif
 }
