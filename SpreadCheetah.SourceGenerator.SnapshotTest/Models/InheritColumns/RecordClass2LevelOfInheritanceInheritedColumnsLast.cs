fcf16b84f2d6eb292e58ddfc35354d47a107e9c3
kobyakov <kobyakov@segmento.ru>
2024-04-26 11:19:46 +0300

    add test for case when class set InheritedColumnsLast and parent class inherit columns

---
+++ SpreadCheetah.SourceGenerator.SnapshotTest/Models/InheritColumns/RecordClass2LevelOfInheritanceInheritedColumnsLast.cs
@@ -0,0 +1,7 @@
+using SpreadCheetah.SourceGeneration;
+
+namespace SpreadCheetah.SourceGenerator.SnapshotTest.Models.InheritColumns;
+
+[InheritColumns(InheritedColumnOrder.InheritedColumnsLast)]
+public record RecordClass2LevelOfInheritanceInheritedColumnsLast(string OwnProperty, bool Value, string ClassProperty)
+    : RecordClassWithInheritedColumnsFirst(Value, ClassProperty);
