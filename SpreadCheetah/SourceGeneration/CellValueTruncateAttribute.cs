493f48909403ab4e1e2c059bd7fa98e2d0c7ec76
sveinungf <ikonage@gmail.com>
2024-05-26 12:37:56 +0200

    Rename CellValueLengthLimit -> CellValueTruncate

--- SpreadCheetah/SourceGeneration/CellValueTruncateAttribute.cs
+++ SpreadCheetah/SourceGeneration/CellValueTruncateAttribute.cs
@@ -1,4 +1,4 @@
 namespace SpreadCheetah.SourceGeneration;
 
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
-public sealed class CellValueLengthLimitAttribute(int length) : Attribute;
+public sealed class CellValueTruncateAttribute(int length) : Attribute;
