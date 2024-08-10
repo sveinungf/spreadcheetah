3b8845bcac0dc5dc8da07058ccb2ce72eca19285
sveinungf <ikonage@gmail.com>
2024-08-11 00:59:51 +0200

    XML doc for StyleNameVisibility

--- SpreadCheetah/Styling/StyleNameVisibility.cs
+++ SpreadCheetah/Styling/StyleNameVisibility.cs
@@ -1,7 +1,13 @@
 namespace SpreadCheetah.Styling;
 
+/// <summary>
+/// Value that specifies the visibility of a style name in the Excel UI.
+/// </summary>
 public enum StyleNameVisibility
 {
+    /// <summary>Visible in the Excel UI.</summary>
     Visible,
+
+    /// <summary>Hidden from the Excel UI.</summary>
     Hidden
 }
