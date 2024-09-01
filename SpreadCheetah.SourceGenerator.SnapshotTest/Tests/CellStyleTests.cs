f182e52462eb7d560550607d7e639700f7b0a2c6
sveinungf <ikonage@gmail.com>
2024-09-01 09:54:25 +0200

    Snapshot tests for CellStyle

---
+++ SpreadCheetah.SourceGenerator.SnapshotTest/Tests/CellStyleTests.cs
@@ -0,0 +1,79 @@
+using SpreadCheetah.SourceGenerator.SnapshotTest.Helpers;
+using SpreadCheetah.SourceGenerators;
+
+namespace SpreadCheetah.SourceGenerator.SnapshotTest.Tests;
+
+public class CellStyleTests
+{
+    [Fact]
+    public Task CellStyle_ClassWithMultipleAttributes()
+    {
+        // Arrange
+        const string source = """
+            using SpreadCheetah.SourceGeneration;
+
+            namespace MyNamespace;
+
+            public class ClassWithCellStyle
+            {
+                [CellStyle("Italic")]
+                public string? FirstName { get; set; }
+            
+                [CellStyle("Bold")]
+                public string? LastName { get; set; }
+            
+                public int YearOfBirth { get; set; }
+            
+                [CellStyle("Bold")]
+                public string? Initials { get; set; }
+            }
+            
+            [WorksheetRow(typeof(ClassWithCellStyle))]
+            public partial class MyGenRowContext : WorksheetRowContext;
+            """;
+
+        // Act & Assert
+        return TestHelper.CompileAndVerify<WorksheetRowGenerator>(source);
+    }
+
+    [Fact]
+    public Task CellStyle_ContextWithTwoClassesWithCellStyle()
+    {
+        // Arrange
+        const string source = """
+            using SpreadCheetah.SourceGeneration;
+
+            namespace MyNamespace;
+
+            public class ClassWithCellStyle
+            {
+                [CellStyle("Italic")]
+                public string? FirstName { get; set; }
+            
+                [CellStyle("Bold")]
+                public string? LastName { get; set; }
+            
+                public int YearOfBirth { get; set; }
+            
+                [CellStyle("Bold")]
+                public string? Initials { get; set; }
+            }
+
+            public class Class2WithCellStyle
+            {
+                [CellStyle("Red")]
+                public string? Id { get; set; }
+            
+                [CellStyle("Bold")]
+                public decimal Price { get; set; }
+            }
+            
+            [WorksheetRow(typeof(ClassWithCellStyle))]
+            [WorksheetRow(typeof(Class2WithCellStyle))]
+            public partial class MyGenRowContext : WorksheetRowContext;
+            """;
+
+        // Act & Assert
+        return TestHelper.CompileAndVerify<WorksheetRowGenerator>(source);
+    }
+}
