0bd883bfd6ff97d6e4e038d612f5ce63aeaebe17
sveinungf <ikonage@gmail.com>
2024-09-06 10:59:13 +0200

    Update XML comments and suppressions for CellValueConverter classes

--- SpreadCheetah/SourceGeneration/CellValueConverter.cs
+++ SpreadCheetah/SourceGeneration/CellValueConverter.cs
@@ -1,17 +1,12 @@
 namespace SpreadCheetah.SourceGeneration;
 
 /// <summary>
-/// Provide the ability to create a custom cell when using source generator.
+/// Converts a value to a <see cref="DataCell"/>. Used in conjunction with <see cref="CellValueConverterAttribute"/>.
 /// </summary>
-/// <typeparam name="T"></typeparam>
-#pragma warning disable S1694
 public abstract class CellValueConverter<T>
-#pragma warning restore S1694
 {
     /// <summary>
-    /// Map provided value to a cell.
+    /// Converts a value to a <see cref="DataCell"/>.
     /// </summary>
-    /// <param name="value">The value to wire in cell.</param>
-    /// <returns></returns>
     public abstract DataCell ConvertToCell(T value);
 }
--- SpreadCheetah/SourceGeneration/CellValueConverterAttribute.cs
+++ SpreadCheetah/SourceGeneration/CellValueConverterAttribute.cs
@@ -1,10 +1,9 @@
 namespace SpreadCheetah.SourceGeneration;
 
 /// <summary>
-/// Instructs the SpreadCheetah source generator to use specified cell mapper to create a cell.
+/// Instructs the SpreadCheetah source generator to convert property values to cells using the specified type.
+/// The specified converter type must derive from <see cref="CellValueConverter{T}"/>,
+/// and its type parameter must match the property type.
 /// </summary>
-[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
-public sealed class CellValueConverterAttribute(Type CellValueConverterType) : Attribute
-{
-
-}
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+public sealed class CellValueConverterAttribute(Type converterType) : Attribute;
