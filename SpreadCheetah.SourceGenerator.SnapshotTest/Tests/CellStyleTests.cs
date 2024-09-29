9893e4b6b2acd2254aa249d03fc776412d3a1c0c
sveinungf <ikonage@gmail.com>
2024-09-29 13:29:59 +0200

    Update test for diagnostic

--- SpreadCheetah.SourceGenerator.SnapshotTest/Tests/CellStyleTests.cs
+++ SpreadCheetah.SourceGenerator.SnapshotTest/Tests/CellStyleTests.cs
@@ -78,25 +78,23 @@ public class CellStyleTests
     }
 
     [Fact]
-    public Task CellStyle_ClassWithEmptyCellStyle()
+    public async Task CellStyle_ClassWithEmptyCellStyle()
     {
         // Arrange
-        const string source = """
+        var context = AnalyzerTest.CreateContext();
+        context.TestCode = """
             using SpreadCheetah.SourceGeneration;
-
+        
             namespace MyNamespace;
-
+        
             public class ClassWithCellStyle
             {
-                [CellStyle("")]
+                [CellStyle({|SPCH1006:""|})]
                 public string? FirstName { get; set; }
             }
-            
-            [WorksheetRow(typeof(ClassWithCellStyle))]
-            public partial class MyGenRowContext : WorksheetRowContext;
             """;
 
         // Act & Assert
-        return TestHelper.CompileAndVerify<WorksheetRowGenerator>(source, onlyDiagnostics: true);
+        await context.RunAsync();
     }
 }
