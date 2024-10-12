33e00d0d044275c22401ccc1554d28792adeea2e
sveinungf <ikonage@gmail.com>
2024-10-12 09:37:08 +0200

    Both CellValueConverter attributes in the same file

--- SpreadCheetah/SourceGeneration/CellValueConverterAttribute.cs
+++ SpreadCheetah/SourceGeneration/CellValueConverterAttribute.cs
@@ -7,3 +7,11 @@ namespace SpreadCheetah.SourceGeneration;
 /// </summary>
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
 public sealed class CellValueConverterAttribute(Type converterType) : Attribute;
+
+/// <summary>
+/// Instructs the SpreadCheetah source generator to convert property values to cells using the specified type.
+/// The specified converter type must derive from <see cref="CellValueConverter{T}"/>,
+/// and its type parameter must match the property type.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+public sealed class CellValueConverterAttribute<T> : Attribute where T : class;
--- SpreadCheetah/SourceGeneration/CellValueConverterAttributeOfT.cs
+++
@@ -1,9 +0,0 @@
-namespace SpreadCheetah.SourceGeneration;
-
-/// <summary>
-/// Instructs the SpreadCheetah source generator to convert property values to cells using the specified type.
-/// The specified converter type must derive from <see cref="CellValueConverter{T}"/>,
-/// and its type parameter must match the property type.
-/// </summary>
-[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
-public sealed class CellValueConverterAttribute<T> : Attribute where T : class;
