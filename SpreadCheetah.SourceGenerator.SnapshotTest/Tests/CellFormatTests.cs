b9c56a7edf53a1f37187ab5c16b25e37f0720c89
sveinungf <ikonage@gmail.com>
2024-10-26 21:54:18 +0200

    Tests for diagnostics with CellFormatAttribute

--- SpreadCheetah.SourceGenerator.SnapshotTest/Tests/CellFormatTests.cs
+++ SpreadCheetah.SourceGenerator.SnapshotTest/Tests/CellFormatTests.cs
@@ -36,4 +36,57 @@ public class CellFormatTests
         // Act & Assert
         return TestHelper.CompileAndVerify<WorksheetRowGenerator>(source);
     }
+
+    [Fact]
+    public Task CellFormat_ClassWithInvalidFormats()
+    {
+        // Arrange
+        var context = AnalyzerTest.CreateContext();
+        context.TestCode = """
+            using SpreadCheetah.SourceGeneration;
+            using SpreadCheetah.Styling;
+
+            namespace MyNamespace;
+
+            public class ClassWithCellFormat
+            {
+                [CellFormat({|SPCH1006:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"|})]
+                public decimal First { get; set; }
+
+                [CellFormat({|SPCH1006:(StandardNumberFormat)100|})]
+                public decimal? Second { get; set; }
+            }
+            
+            [WorksheetRow(typeof(ClassWithCellFormat))]
+            public partial class MyGenRowContext : WorksheetRowContext;
+            """;
+
+        // Act & Assert
+        return context.RunAsync();
+    }
+
+    [Fact]
+    public Task CellFormat_ClassPropertyWithCellFormatAndCellStyle()
+    {
+        // Arrange
+        var context = AnalyzerTest.CreateContext();
+        context.TestCode = """
+            using SpreadCheetah.SourceGeneration;
+
+            namespace MyNamespace;
+
+            public class ClassWithCellFormat
+            {
+                [CellFormat("#.0#")]
+                [{|SPCH1008:CellStyle("My style")|}]
+                public decimal Value { get; set; }
+            }
+            
+            [WorksheetRow(typeof(ClassWithCellFormat))]
+            public partial class MyGenRowContext : WorksheetRowContext;
+            """;
+
+        // Act & Assert
+        return context.RunAsync();
+    }
 }
