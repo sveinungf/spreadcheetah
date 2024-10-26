1826c0ac0ce58a16b2daa833e6acf55f1016eaee
sveinungf <ikonage@gmail.com>
2024-10-26 22:33:32 +0200

    XML comments for CellFormatAttribute

--- SpreadCheetah/SourceGeneration/CellFormatAttribute.cs
+++ SpreadCheetah/SourceGeneration/CellFormatAttribute.cs
@@ -2,13 +2,22 @@ using SpreadCheetah.Styling;
 
 namespace SpreadCheetah.SourceGeneration;
 
+/// <summary>
+/// Instructs the SpreadCheetah source generator to apply a number format style to the cells created for a property.
+/// </summary>
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
 public sealed class CellFormatAttribute : Attribute
 {
+    /// <summary>
+    /// Instructs the SpreadCheetah source generator to apply a custom number format style to the cells created for a property.
+    /// </summary>
     public CellFormatAttribute(string customFormat)
     {
     }
 
+    /// <summary>
+    /// Instructs the SpreadCheetah source generator to apply a standard number format style to the cells created for a property.
+    /// </summary>
     public CellFormatAttribute(StandardNumberFormat format)
     {
     }
