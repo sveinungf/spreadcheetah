bef76f6c377fe172a1d788e52de11b5df053bad4
sveinungf <ikonage@gmail.com>
2024-11-03 00:17:19 +0100

    Tests for ColumnIgnore diagnostics

---
+++ SpreadCheetah.SourceGenerator.SnapshotTest/Tests/ColumnIgnoreTests.cs
@@ -0,0 +1,57 @@
+using SpreadCheetah.SourceGenerator.SnapshotTest.Helpers;
+
+namespace SpreadCheetah.SourceGenerator.SnapshotTest.Tests;
+
+public class ColumnIgnoreTests
+{
+    [Fact]
+    public Task ColumnIgnore_ClassPropertyWithAnotherSpreadCheetahAttribute()
+    {
+        // Arrange
+        var context = AnalyzerTest.CreateContext();
+        context.TestCode = """
+            using SpreadCheetah.SourceGeneration;
+
+            namespace MyNamespace;
+
+            public class ClassWithColumnIgnore
+            {
+                [{|SPCH1008:CellFormat("#.0#")|}]
+                [ColumnIgnore]
+                public decimal Value { get; set; }
+            }
+            
+            [WorksheetRow(typeof(ClassWithColumnIgnore))]
+            public partial class MyGenRowContext : WorksheetRowContext;
+            """;
+
+        // Act & Assert
+        return context.RunAsync();
+    }
+
+    [Fact]
+    public Task ColumnIgnore_ClassPropertyWithAnotherUnrelatedAttribute()
+    {
+        // Arrange
+        var context = AnalyzerTest.CreateContext();
+        context.TestCode = """
+            using SpreadCheetah.SourceGeneration;
+            using System.ComponentModel.DataAnnotations;
+
+            namespace MyNamespace;
+
+            public class ClassWithColumnIgnore
+            {
+                [Required]
+                [ColumnIgnore]
+                public decimal Value { get; set; }
+            }
+            
+            [WorksheetRow(typeof(ClassWithColumnIgnore))]
+            public partial class MyGenRowContext : WorksheetRowContext;
+            """;
+
+        // Act & Assert
+        return context.RunAsync();
+    }
+}
