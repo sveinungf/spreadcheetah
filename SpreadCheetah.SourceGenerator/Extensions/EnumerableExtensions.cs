cc279fc2c9882affd1a399b433b3caeab987f5b6
sveinungf <ikonage@gmail.com>
2024-12-21 22:23:50 +0100

    Replace PolySharp with Polyfill

--- SpreadCheetah.SourceGenerator/Extensions/EnumerableExtensions.cs
+++ SpreadCheetah.SourceGenerator/Extensions/EnumerableExtensions.cs
@@ -4,11 +4,6 @@ namespace SpreadCheetah.SourceGenerator.Extensions;
 
 internal static class EnumerableExtensions
 {
-    public static IEnumerable<(int Index, T Element)> Index<T>(this IEnumerable<T> elements)
-    {
-        return elements.Select((x, i) => (i, x));
-    }
-
     public static EquatableArray<T> ToEquatableArray<T>(this IEnumerable<T> elements)
         where T : IEquatable<T>
     {
