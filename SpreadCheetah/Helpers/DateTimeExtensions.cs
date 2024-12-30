2f215f4f18a338a15a93eb0f242ca096347987cb
sveinungf <ikonage@gmail.com>
2024-12-30 16:29:14 +0100

    Remove unused extension

--- SpreadCheetah/Helpers/DateTimeExtensions.cs
+++
@@ -1,9 +0,0 @@
-using System.Globalization;
-
-namespace SpreadCheetah.Helpers;
-
-internal static class DateTimeExtensions
-{
-    public static string ToStringInvariant(this DateTime n) => n.ToString(CultureInfo.InvariantCulture);
-
-}
