db0237e04cbb9efcbec27560b3237f284cf339cd
sveinungf <ikonage@gmail.com>
2025-02-24 00:39:36 +0100

    WIP for validating table name

--- SpreadCheetah/Tables/Table.cs
+++ SpreadCheetah/Tables/Table.cs
@@ -14,7 +14,30 @@ public sealed class Table
     public Table(TableStyle style, string? name = null)
     {
         Style = Guard.DefinedEnumValue(style);
-        Name = name; // TODO: Validate
+
+        if (name is not null)
+            EnsureValidTableName(name);
+
+        Name = name;
+    }
+
+    private static void EnsureValidTableName(string name)
+    {
+        if (name.Length == 0)
+            throw new NotImplementedException(); // TODO: Empty string not allowed.
+        if (name.Length > 255)
+            throw new NotImplementedException(); // TODO: A table name can have up to 255 characters.
+        if (name is "C" or "c" or "R" or "r")
+            throw new NotImplementedException(); // TODO: You can't use "C", "c", "R", or "r" for the name.
+
+        // TODO: Name must start with a letter, underscore, or '\'.
+        // TODO: Use letters, numbers, periods, and underscore characters for the rest of the name (and also '\')
+        if (!Regexes.TableNameValidCharacters().IsMatch(name))
+            throw new NotImplementedException();
+
+        // TODO: Don't allow A1 or R1C1 references (also with different casing)
+        if (Regexes.TableNameCellReference().IsMatch(name))
+            throw new NotImplementedException();
     }
 
     internal Dictionary<int, TableColumnOptions>? ColumnOptions { get; private set; }
