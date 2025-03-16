4941795f36f552593bb39291bccad41ecd4e2dfa
sveinungf <ikonage@gmail.com>
2025-03-16 13:14:11 +0100

    Expose XmlUtility for tests without using InternalsVisibleTo

--- SpreadCheetah/Helpers/XmlUtility.cs
+++ SpreadCheetah/Helpers/XmlUtility.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace SpreadCheetah.Helpers;
 
-internal static class XmlUtility
+#if INTERNALS_EXPOSED_FOR_TESTS
+public
+#else
+internal
+#endif
+static class XmlUtility
 {
 #if NET8_0_OR_GREATER
     private static ReadOnlySpan<char> CharsToEscape =>
--- SpreadCheetah/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-[assembly: CLSCompliant(true)]
-
-#if DEBUG
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("SpreadCheetah.Test")]
-#endif
