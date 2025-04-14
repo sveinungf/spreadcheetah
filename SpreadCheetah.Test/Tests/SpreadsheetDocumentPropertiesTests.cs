072e287e5751d06c6320ec264ca7429a9b480b1d
sveinungf <ikonage@gmail.com>
2025-04-14 17:05:17 +0200

    More tests for docProps

--- SpreadCheetah.Test/Tests/SpreadsheetDocumentPropertiesTests.cs
+++ SpreadCheetah.Test/Tests/SpreadsheetDocumentPropertiesTests.cs
@@ -15,6 +15,7 @@ public class SpreadsheetDocumentPropertiesTests
 
         var options = new SpreadCheetahOptions
         {
+            BufferSize = SpreadCheetahOptions.MinimumBufferSize,
             DocumentProperties = new DocumentProperties
             {
                 Author = author,
@@ -35,6 +36,51 @@ public class SpreadsheetDocumentPropertiesTests
         Assert.Equal(author, actual.Author);
         Assert.Equal(subject, actual.Subject);
         Assert.Equal(title, actual.Title);
+        var actualCreated = Assert.NotNull(actual.Created);
+        Assert.Equal(DateTime.UtcNow, actualCreated, TimeSpan.FromSeconds(5));
+    }
+
+    [Fact]
+    public async Task Spreadsheet_DocumentProperties_ExplicitlySetToNull()
+    {
+        // Arrange
+        var options = new SpreadCheetahOptions
+        {
+            DocumentProperties = null
+        };
+
+        using var stream = new MemoryStream();
+
+        // Act
+        await using var spreadsheet = await Spreadsheet.CreateNewAsync(stream, options);
+        await spreadsheet.StartWorksheetAsync("Sheet");
+        await spreadsheet.FinishAsync();
+
+        // Assert
+        var actual = SpreadsheetAssert.DocumentProperties(stream);
+        Assert.Null(actual.Author);
+        Assert.Null(actual.Subject);
+        Assert.Null(actual.Title);
+        Assert.Null(actual.Created);
+    }
+
+    [Theory, CombinatorialData]
+    public async Task Spreadsheet_DocumentProperties_Default(bool withOptions)
+    {
+        // Arrange
+        SpreadCheetahOptions? options = withOptions ? new() : null;
+        using var stream = new MemoryStream();
+
+        // Act
+        await using var spreadsheet = await Spreadsheet.CreateNewAsync(stream, options);
+        await spreadsheet.StartWorksheetAsync("Sheet");
+        await spreadsheet.FinishAsync();
+
+        // Assert
+        var actual = SpreadsheetAssert.DocumentProperties(stream);
+        Assert.Null(actual.Author);
+        Assert.Null(actual.Subject);
+        Assert.Null(actual.Title);
         var actualCreated = Assert.NotNull(actual.Created);
         Assert.Equal(DateTime.UtcNow, actualCreated, TimeSpan.FromSeconds(5));
     }
