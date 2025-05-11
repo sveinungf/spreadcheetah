a25f4073d6f888d0d1410353c1a3d991bbde239a
sveinungf <ikonage@gmail.com>
2025-05-11 22:39:56 +0200

    WIP for Formula.Hyperlink

--- SpreadCheetah/Formula.cs
+++ SpreadCheetah/Formula.cs
@@ -1,3 +1,5 @@
+using SpreadCheetah.Helpers;
+
 namespace SpreadCheetah;
 
 /// <summary>
@@ -14,5 +16,44 @@ public readonly record struct Formula
     public Formula(string? formulaText)
     {
         FormulaText = formulaText ?? "";
+    }
+
+    public static Formula Hyperlink(Uri uri)
+    {
+        var absoluteUri = EnsureValidAbsoluteUri(uri);
+        var xmlEncodedUri = XmlUtility.XmlEncode(absoluteUri);
+        return new Formula($"""HYPERLINK("{xmlEncodedUri}")""");
+    }
+
+    public static Formula Hyperlink(Uri uri, string friendlyName)
+    {
+        var absoluteUri = EnsureValidAbsoluteUri(uri);
+
+        // TODO: Use throw helpers
+        if (friendlyName is null)
+            throw new ArgumentNullException(nameof(friendlyName));
+        if (friendlyName.Length > 255)
+            throw new ArgumentException("The URI friendly name can not exceed 255 characters.", nameof(uri));
+
+        var xmlEncodedUri = XmlUtility.XmlEncode(absoluteUri);
+        var xmlEncodedFriendlyName = XmlUtility.XmlEncode(friendlyName);
+        return new Formula($"""HYPERLINK("{xmlEncodedUri}","{xmlEncodedFriendlyName}")""");
+    }
+
+    private static string EnsureValidAbsoluteUri(Uri uri)
+    {
+        // TODO: Use throw helpers
+        if (uri is null)
+            throw new ArgumentNullException(nameof(uri));
+        if (!uri.IsAbsoluteUri)
+            throw new ArgumentException("The URI must be an absolute URI.", nameof(uri)); // TODO: Covered by IsWellFormedOriginalString?
+        if (!uri.IsWellFormedOriginalString())
+            throw new ArgumentException("The URI is not well formed.", nameof(uri));
+
+        var absoluteUri = uri.AbsoluteUri;
+        if (absoluteUri.Length > 255)
+            throw new ArgumentException("The URI can not exceed 255 characters.", nameof(uri));
+
+        return absoluteUri;
     }
 }
