c71992bc907036c6bf9a0e58e1b38225eedabc21
sveinungf <ikonage@gmail.com>
2025-05-17 00:15:41 +0200

    Move Hyperlink Formula implementation to separate file

--- SpreadCheetah/Formula.cs
+++ SpreadCheetah/Formula.cs
@@ -1,8 +1,4 @@
-using SpreadCheetah.Helpers;
-
-#if !NET6_0_OR_GREATER
-using ArgumentNullException = SpreadCheetah.Helpers.Backporting.ArgumentNullExceptionBackport;
-#endif
+using SpreadCheetah.Formulas;
 
 namespace SpreadCheetah;
 
@@ -22,37 +18,7 @@ public readonly record struct Formula
         FormulaText = formulaText ?? "";
     }
 
-    public static Formula Hyperlink(Uri uri)
-    {
-        var absoluteUri = EnsureValidAbsoluteUri(uri);
-        var xmlEncodedUri = XmlUtility.XmlEncode(absoluteUri);
-        return new Formula($"""HYPERLINK("{xmlEncodedUri}")""");
-    }
+    public static Formula Hyperlink(Uri uri) => HyperlinkFormula.From(uri);
 
-    public static Formula Hyperlink(Uri uri, string friendlyName)
-    {
-        ArgumentNullException.ThrowIfNull(friendlyName);
-        if (friendlyName.Length > 255)
-            ThrowHelper.HyperlinkFriendlyNameTooLong(nameof(friendlyName));
-
-        var absoluteUri = EnsureValidAbsoluteUri(uri);
-        var xmlEncodedUri = XmlUtility.XmlEncode(absoluteUri);
-        var xmlEncodedFriendlyName = XmlUtility.XmlEncode(friendlyName);
-        return new Formula($"""HYPERLINK("{xmlEncodedUri}","{xmlEncodedFriendlyName}")""");
-    }
-
-    private static string EnsureValidAbsoluteUri(Uri uri)
-    {
-        ArgumentNullException.ThrowIfNull(uri);
-        if (!uri.IsAbsoluteUri)
-            ThrowHelper.UriMustBeAbsolute(nameof(uri));
-        if (!uri.IsWellFormedOriginalString())
-            ThrowHelper.UriMustBeWellFormed(nameof(uri));
-
-        var absoluteUri = uri.AbsoluteUri;
-        if (absoluteUri.Length > 255)
-            ThrowHelper.UriTooLong(nameof(uri));
-
-        return absoluteUri;
-    }
+    public static Formula Hyperlink(Uri uri, string friendlyName) => HyperlinkFormula.From(uri, friendlyName);
 }
---
+++ SpreadCheetah/Formulas/HyperlinkFormula.cs
@@ -0,0 +1,44 @@
+using SpreadCheetah.Helpers;
+
+#if !NET6_0_OR_GREATER
+using ArgumentNullException = SpreadCheetah.Helpers.Backporting.ArgumentNullExceptionBackport;
+#endif
+
+namespace SpreadCheetah.Formulas;
+
+internal static class HyperlinkFormula
+{
+    public static Formula From(Uri uri)
+    {
+        var absoluteUri = EnsureValidAbsoluteUri(uri);
+        var xmlEncodedUri = XmlUtility.XmlEncode(absoluteUri);
+        return new Formula($"""HYPERLINK("{xmlEncodedUri}")""");
+    }
+
+    public static Formula From(Uri uri, string friendlyName)
+    {
+        ArgumentNullException.ThrowIfNull(friendlyName);
+        if (friendlyName.Length > 255)
+            ThrowHelper.HyperlinkFriendlyNameTooLong(nameof(friendlyName));
+
+        var absoluteUri = EnsureValidAbsoluteUri(uri);
+        var xmlEncodedUri = XmlUtility.XmlEncode(absoluteUri);
+        var xmlEncodedFriendlyName = XmlUtility.XmlEncode(friendlyName);
+        return new Formula($"""HYPERLINK("{xmlEncodedUri}","{xmlEncodedFriendlyName}")""");
+    }
+
+    private static string EnsureValidAbsoluteUri(Uri uri)
+    {
+        ArgumentNullException.ThrowIfNull(uri);
+        if (!uri.IsAbsoluteUri)
+            ThrowHelper.UriMustBeAbsolute(nameof(uri));
+        if (!uri.IsWellFormedOriginalString())
+            ThrowHelper.UriMustBeWellFormed(nameof(uri));
+
+        var absoluteUri = uri.AbsoluteUri;
+        if (absoluteUri.Length > 255)
+            ThrowHelper.UriTooLong(nameof(uri));
+
+        return absoluteUri;
+    }
+}
