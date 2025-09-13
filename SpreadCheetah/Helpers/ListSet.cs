23387049628cd47aba75b0f96a614ea106336551
sveinungf <ikonage@gmail.com>
2025-09-14 00:40:24 +0200

    Use Dictionary for lookup in ListSet.Add

--- SpreadCheetah/Helpers/ListSet.cs
+++ SpreadCheetah/Helpers/ListSet.cs
@@ -2,20 +2,22 @@ using SpreadCheetah.Styling;
 
 namespace SpreadCheetah.Helpers;
 
-internal sealed class ListSet<T>
+internal sealed class ListSet<T> where T : notnull
 {
     private readonly List<T> _list = [];
+    private readonly Dictionary<T, int> _dictionary = [];
 
     public List<T> GetList() => _list;
 
     public int Add(in T item)
     {
-        var index = _list.IndexOf(item);
-        if (index != -1)
+        if (_dictionary.TryGetValue(item, out var index))
             return index;
 
+        var newIndex = _list.Count;
         _list.Add(item);
-        return _list.Count - 1;
+        _dictionary[item] = newIndex;
+        return newIndex;
     }
 }
 
