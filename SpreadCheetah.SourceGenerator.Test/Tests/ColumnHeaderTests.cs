4812efb38addf872db9d7bfda2ea78a9d475bafc
sveinungf <ikonage@gmail.com>
2025-10-25 19:31:01 +0200

    Simplify ColumnHeaderTests

--- SpreadCheetah.SourceGenerator.Test/Tests/ColumnHeaderTests.cs
+++ SpreadCheetah.SourceGenerator.Test/Tests/ColumnHeaderTests.cs
@@ -11,49 +11,46 @@ public class ColumnHeaderTests
     public void ColumnHeader_ClassWithPropertyReferenceColumnHeaders_CanReadTypeAndPropertyName()
     {
         // Arrange
-        var publicProperties = typeof(ClassWithPropertyReferenceColumnHeaders).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-        var firstNameProperty = publicProperties.SingleOrDefault(p => string.Equals(p.Name, nameof(ClassWithPropertyReferenceColumnHeaders.FirstName), StringComparison.Ordinal));
-        var lastNameProperty = publicProperties.SingleOrDefault(p => string.Equals(p.Name, nameof(ClassWithPropertyReferenceColumnHeaders.LastName), StringComparison.Ordinal));
-        var nationalityProperty = publicProperties.SingleOrDefault(p => string.Equals(p.Name, nameof(ClassWithPropertyReferenceColumnHeaders.Nationality), StringComparison.Ordinal));
-        var addressLine1Property = publicProperties.SingleOrDefault(p => string.Equals(p.Name, nameof(ClassWithPropertyReferenceColumnHeaders.AddressLine1), StringComparison.Ordinal));
-        var addressLine2Property = publicProperties.SingleOrDefault(p => string.Equals(p.Name, nameof(ClassWithPropertyReferenceColumnHeaders.AddressLine2), StringComparison.Ordinal));
-        var ageProperty = publicProperties.SingleOrDefault(p => string.Equals(p.Name, nameof(ClassWithPropertyReferenceColumnHeaders.Age), StringComparison.Ordinal));
+        var publicProperties = typeof(ClassWithPropertyReferenceColumnHeaders)
+            .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+            .ToDictionary(x => x.Name, StringComparer.Ordinal);
+
+        var firstNameProperty = publicProperties[nameof(ClassWithPropertyReferenceColumnHeaders.FirstName)];
+        var lastNameProperty = publicProperties[nameof(ClassWithPropertyReferenceColumnHeaders.LastName)];
+        var nationalityProperty = publicProperties[nameof(ClassWithPropertyReferenceColumnHeaders.Nationality)];
+        var addressLine1Property = publicProperties[nameof(ClassWithPropertyReferenceColumnHeaders.AddressLine1)];
+        var addressLine2Property = publicProperties[nameof(ClassWithPropertyReferenceColumnHeaders.AddressLine2)];
+        var ageProperty = publicProperties[nameof(ClassWithPropertyReferenceColumnHeaders.Age)];
 
         // Act
-        var firstNameColHeaderAttr = firstNameProperty?.GetCustomAttribute<ColumnHeaderAttribute>();
-        var lastNameColHeaderAttr = lastNameProperty?.GetCustomAttribute<ColumnHeaderAttribute>();
-        var nationalityColHeaderAttr = nationalityProperty?.GetCustomAttribute<ColumnHeaderAttribute>();
-        var addressLine1ColHeaderAttr = addressLine1Property?.GetCustomAttribute<ColumnHeaderAttribute>();
-        var addressLine2ColHeaderAttr = addressLine2Property?.GetCustomAttribute<ColumnHeaderAttribute>();
-        var ageColHeaderAttr = ageProperty?.GetCustomAttribute<ColumnHeaderAttribute>();
+        var firstNameColHeaderAttr = firstNameProperty.GetCustomAttribute<ColumnHeaderAttribute>();
+        var lastNameColHeaderAttr = lastNameProperty.GetCustomAttribute<ColumnHeaderAttribute>();
+        var nationalityColHeaderAttr = nationalityProperty.GetCustomAttribute<ColumnHeaderAttribute>();
+        var addressLine1ColHeaderAttr = addressLine1Property.GetCustomAttribute<ColumnHeaderAttribute>();
+        var addressLine2ColHeaderAttr = addressLine2Property.GetCustomAttribute<ColumnHeaderAttribute>();
+        var ageColHeaderAttr = ageProperty.GetCustomAttribute<ColumnHeaderAttribute>();
 
         // Assert
-        Assert.NotNull(firstNameProperty);
         Assert.NotNull(firstNameColHeaderAttr);
         Assert.Equal(typeof(ColumnHeaderResources), firstNameColHeaderAttr.Type);
         Assert.Equal(nameof(ColumnHeaderResources.Header_FirstName), firstNameColHeaderAttr.PropertyName);
 
-        Assert.NotNull(lastNameProperty);
         Assert.NotNull(lastNameColHeaderAttr);
         Assert.Equal(typeof(ColumnHeaderResources), lastNameColHeaderAttr.Type);
         Assert.Equal(nameof(ColumnHeaderResources.Header_LastName), lastNameColHeaderAttr.PropertyName);
 
-        Assert.NotNull(nationalityProperty);
         Assert.NotNull(nationalityColHeaderAttr);
         Assert.Equal(typeof(ColumnHeaders), nationalityColHeaderAttr.Type);
         Assert.Equal(nameof(ColumnHeaders.HeaderNationality), nationalityColHeaderAttr.PropertyName);
 
-        Assert.NotNull(addressLine1Property);
         Assert.NotNull(addressLine1ColHeaderAttr);
         Assert.Equal(typeof(ColumnHeaders), addressLine1ColHeaderAttr.Type);
         Assert.Equal(nameof(ColumnHeaders.HeaderAddressLine1), addressLine1ColHeaderAttr.PropertyName);
 
-        Assert.NotNull(addressLine2Property);
         Assert.NotNull(addressLine2ColHeaderAttr);
         Assert.Equal(typeof(ColumnHeaders), addressLine2ColHeaderAttr.Type);
         Assert.Equal(nameof(ColumnHeaders.HeaderAddressLine2), addressLine2ColHeaderAttr.PropertyName);
 
-        Assert.NotNull(ageProperty);
         Assert.NotNull(ageColHeaderAttr);
         Assert.Equal(typeof(ColumnHeaders), ageColHeaderAttr.Type);
         Assert.Equal(nameof(ColumnHeaders.HeaderAge), ageColHeaderAttr.PropertyName);
@@ -63,25 +60,28 @@ public class ColumnHeaderTests
     public void ColumnHeader_ClassWithSpecialCharacterColumnHeaders_CanReadName()
     {
         // Arrange
-        var publicProperties = typeof(ClassWithSpecialCharacterColumnHeaders).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-        var firstNameProperty = publicProperties.SingleOrDefault(p => string.Equals(p.Name, nameof(ClassWithSpecialCharacterColumnHeaders.FirstName), StringComparison.Ordinal));
-        var lastNameProperty = publicProperties.SingleOrDefault(p => string.Equals(p.Name, nameof(ClassWithSpecialCharacterColumnHeaders.LastName), StringComparison.Ordinal));
-        var nationalityProperty = publicProperties.SingleOrDefault(p => string.Equals(p.Name, nameof(ClassWithSpecialCharacterColumnHeaders.Nationality), StringComparison.Ordinal));
-        var addressLine1Property = publicProperties.SingleOrDefault(p => string.Equals(p.Name, nameof(ClassWithSpecialCharacterColumnHeaders.AddressLine1), StringComparison.Ordinal));
-        var addressLine2Property = publicProperties.SingleOrDefault(p => string.Equals(p.Name, nameof(ClassWithSpecialCharacterColumnHeaders.AddressLine2), StringComparison.Ordinal));
-        var ageProperty = publicProperties.SingleOrDefault(p => string.Equals(p.Name, nameof(ClassWithSpecialCharacterColumnHeaders.Age), StringComparison.Ordinal));
-        var noteProperty = publicProperties.SingleOrDefault(p => string.Equals(p.Name, nameof(ClassWithSpecialCharacterColumnHeaders.Note), StringComparison.Ordinal));
-        var note2Property = publicProperties.SingleOrDefault(p => string.Equals(p.Name, nameof(ClassWithSpecialCharacterColumnHeaders.Note2), StringComparison.Ordinal));
+        var publicProperties = typeof(ClassWithSpecialCharacterColumnHeaders)
+            .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+            .ToDictionary(x => x.Name, StringComparer.Ordinal);
+
+        var firstNameProperty = publicProperties[nameof(ClassWithSpecialCharacterColumnHeaders.FirstName)];
+        var lastNameProperty = publicProperties[nameof(ClassWithSpecialCharacterColumnHeaders.LastName)];
+        var nationalityProperty = publicProperties[nameof(ClassWithSpecialCharacterColumnHeaders.Nationality)];
+        var addressLine1Property = publicProperties[nameof(ClassWithSpecialCharacterColumnHeaders.AddressLine1)];
+        var addressLine2Property = publicProperties[nameof(ClassWithSpecialCharacterColumnHeaders.AddressLine2)];
+        var ageProperty = publicProperties[nameof(ClassWithSpecialCharacterColumnHeaders.Age)];
+        var noteProperty = publicProperties[nameof(ClassWithSpecialCharacterColumnHeaders.Note)];
+        var note2Property = publicProperties[nameof(ClassWithSpecialCharacterColumnHeaders.Note2)];
 
         // Act
-        var firstNameColHeaderAttr = firstNameProperty?.GetCustomAttribute<ColumnHeaderAttribute>();
-        var lastNameColHeaderAttr = lastNameProperty?.GetCustomAttribute<ColumnHeaderAttribute>();
-        var nationalityColHeaderAttr = nationalityProperty?.GetCustomAttribute<ColumnHeaderAttribute>();
-        var addressLine1ColHeaderAttr = addressLine1Property?.GetCustomAttribute<ColumnHeaderAttribute>();
-        var addressLine2ColHeaderAttr = addressLine2Property?.GetCustomAttribute<ColumnHeaderAttribute>();
-        var ageColHeaderAttr = ageProperty?.GetCustomAttribute<ColumnHeaderAttribute>();
-        var noteColHeaderAttr = noteProperty?.GetCustomAttribute<ColumnHeaderAttribute>();
-        var note2ColHeaderAttr = note2Property?.GetCustomAttribute<ColumnHeaderAttribute>();
+        var firstNameColHeaderAttr = firstNameProperty.GetCustomAttribute<ColumnHeaderAttribute>();
+        var lastNameColHeaderAttr = lastNameProperty.GetCustomAttribute<ColumnHeaderAttribute>();
+        var nationalityColHeaderAttr = nationalityProperty.GetCustomAttribute<ColumnHeaderAttribute>();
+        var addressLine1ColHeaderAttr = addressLine1Property.GetCustomAttribute<ColumnHeaderAttribute>();
+        var addressLine2ColHeaderAttr = addressLine2Property.GetCustomAttribute<ColumnHeaderAttribute>();
+        var ageColHeaderAttr = ageProperty.GetCustomAttribute<ColumnHeaderAttribute>();
+        var noteColHeaderAttr = noteProperty.GetCustomAttribute<ColumnHeaderAttribute>();
+        var note2ColHeaderAttr = note2Property.GetCustomAttribute<ColumnHeaderAttribute>();
 
         // Assert
         Assert.NotNull(firstNameColHeaderAttr);
