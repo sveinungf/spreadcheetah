d24924e5394d2d8ccf93199df97f424469900617
sveinungf <ikonage@gmail.com>
2025-12-27 22:47:57 +0100

    Use required properties for MixedDataTypeCells record

--- SpreadCheetah.Benchmark/Benchmarks/MixedDataTypeCells.cs
+++ SpreadCheetah.Benchmark/Benchmarks/MixedDataTypeCells.cs
@@ -17,23 +17,28 @@ public class MixedDataTypeCells
         var random = new Random(42);
         var baseDateTime = new DateTime(2000, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-        _rows = Enumerable.Range(0, NumberOfRows)
+        _rows =
+        [
+            .. Enumerable.Range(0, NumberOfRows)
             .Select(row =>
             {
                 var even = row % 2 == 0;
-                return new RowItem(
-                    row,
-                    "Ola-" + row,
-                    random.NextBoolean() ? row + "-Nordmann" : null,
-                    random.NextBoolean() ? null : 1950 + row / 1000,
-                    5.67 + row % 10,
-                    even,
-                    even ? "Norway" : "Sweden",
-                    !even,
-                    random.NextBoolean() ? 0.991f + row / 10000.0 : null,
-                    even ? -23 : 23,
-                    random.NextBoolean() ? baseDateTime.AddSeconds(random.Next()) : null);
-            }).ToArray();
+                return new RowItem
+                {
+                    A = row,
+                    B = "Ola-" + row,
+                    C = random.NextBoolean() ? row + "-Nordmann" : null,
+                    D = random.NextBoolean() ? null : 1950 + row / 1000,
+                    E = 5.67 + row % 10,
+                    F = even,
+                    G = even ? "Norway" : "Sweden",
+                    H = !even,
+                    I = random.NextBoolean() ? 0.991f + row / 10000.0 : null,
+                    J = even ? -23 : 23,
+                    K = random.NextBoolean() ? baseDateTime.AddSeconds(random.Next()) : null
+                };
+            })
+        ];
     }
 
     [Benchmark]
--- SpreadCheetah.Benchmark/Helpers/RowItem.cs
+++ SpreadCheetah.Benchmark/Helpers/RowItem.cs
@@ -1,3 +1,16 @@
 namespace SpreadCheetah.Benchmark.Helpers;
 
-public record RowItem(int A, string B, string? C, int? D, double E, bool F, string G, bool H, double? I, int J, DateTime? K);
+public record RowItem
+{
+    public required int A { get; init; }
+    public required string B { get; init; }
+    public required string? C { get; init; }
+    public required int? D { get; init; }
+    public required double E { get; init; }
+    public required bool F { get; init; }
+    public required string G { get; init; }
+    public required bool H { get; init; }
+    public required double? I { get; init; }
+    public required int J { get; init; }
+    public required DateTime? K { get; init; }
+}
