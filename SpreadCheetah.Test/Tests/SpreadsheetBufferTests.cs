1cc85abdacd48786970ed5ac326319bdbbb73fc4
eroke <ageevda@outlook.com>
2026-02-20 01:20:25 +0300

    color attribute write buffer negative tests

--- SpreadCheetah.Test/Tests/SpreadsheetBufferTests.cs
+++ SpreadCheetah.Test/Tests/SpreadsheetBufferTests.cs
@@ -1,5 +1,6 @@
 using SpreadCheetah.MetadataXml.Attributes;
 using SpreadCheetah.Styling;
+using System.Drawing;
 
 namespace SpreadCheetah.Test.Tests;
 
@@ -9,7 +10,7 @@ public class SpreadsheetBufferTests
     [Theory]
     [InlineData(16, 16)] // No space for ' '
     [InlineData(14, 16)] // No space for attribute name
-    [InlineData(10, 16)] // No space for '=\'
+    [InlineData(10, 16)] // No space for '="'
     [InlineData(8, 16)] // No space for value
     [InlineData(6, 16)] // No space for '"'
     public void IntAttribute_BufferSizeIsNotEnough(int bufferInitialPosition, int bufferSize)
@@ -30,7 +31,7 @@ public class SpreadsheetBufferTests
     [Theory]
     [InlineData(16, 16)] // No space for ' '
     [InlineData(14, 16)] // No space for attribute name
-    [InlineData(10, 16)] // No space for '=\'
+    [InlineData(10, 16)] // No space for '="'
     [InlineData(9, 16)] // No space for value
     [InlineData(8, 16)] // No space for '"'
     public void BooleanAttribute_BufferSizeIsNotEnough(int bufferInitialPosition, int bufferSize)
@@ -51,7 +52,7 @@ public class SpreadsheetBufferTests
     [Theory]
     [InlineData(16, 16)] // No space for ' '
     [InlineData(14, 16)] // No space for attribute name
-    [InlineData(10, 16)] // No space for '=\'
+    [InlineData(10, 16)] // No space for '="'
     [InlineData(9, 16)] // No space for value
     [InlineData(4, 16)] // No space for '"'
     public void SpanByteAttribute_BufferSizeIsNotEnough(int bufferInitialPosition, int bufferSize)
@@ -72,7 +73,7 @@ public class SpreadsheetBufferTests
     [Theory]
     [InlineData(16, 16)] // No space for ' '
     [InlineData(14, 16)] // No space for attribute name
-    [InlineData(10, 16)] // No space for '=\'
+    [InlineData(10, 16)] // No space for '="'
     [InlineData(8, 16)] // No space for value
     [InlineData(7, 16)] // No space for '"'
     public void SimpleSingleCellReferenceAttribute_BufferSizeIsNotEnough(int bufferInitialPosition, int bufferSize)
@@ -81,6 +82,26 @@ public class SpreadsheetBufferTests
         using var buffer = new SpreadsheetBuffer(bufferSize);
         buffer.Advance(bufferInitialPosition);
         var attribute = new SimpleSingleCellReferenceAttribute("name"u8, new CellReferences.SimpleSingleCellReference(1, 2));
+
+        // Act 
+        var result = buffer.TryWrite($"{attribute}");
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Theory]
+    [InlineData(16, 16)] // No space for ' '
+    [InlineData(14, 16)] // No space for attribute name
+    [InlineData(10, 16)] // No space for '="'
+    [InlineData(8, 16)] // No space for value
+    [InlineData(1, 16)] // No space for '"'
+    public void ColorAttribute_BufferSizeIsNotEnough(int bufferInitialPosition, int bufferSize)
+    {
+        // Arrange
+        using var buffer = new SpreadsheetBuffer(bufferSize);
+        buffer.Advance(bufferInitialPosition);
+        var attribute = new ColorAttribute("name"u8, Color.Black);
 
         // Act 
         var result = buffer.TryWrite($"{attribute}");
